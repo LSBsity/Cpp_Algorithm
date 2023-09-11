c8d2030d5810f3635ac4f990dfb9bb20cf4fd34f
LSBsity <126085673+LSBsity@users.noreply.github.com>
2023-09-12 02:38:24 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list)
+{
+    vector<int> answer = {0, 0};
+
+    for (int i = 0; i < num_list.size(); i++)
+    {
+        if (num_list[i] % 2 == 0)
+            answer[0] += 1;
+        else
+            answer[1] += 1;
+    }
+    return answer;
+}
