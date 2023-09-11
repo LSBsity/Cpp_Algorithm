9268e3009ef1790522cea247ba7b31717f7aa7e2
LSBsity <126085673+LSBsity@users.noreply.github.com>
2023-09-12 02:56:55 +0900

    [level 0] Title: 암호 해독, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string cipher, int code)
+{
+    string answer = "";
+
+    for (int i = code - 1; i < cipher.length(); i+=code)
+        answer.push_back(cipher[i]);
+
+    return answer;
+}
