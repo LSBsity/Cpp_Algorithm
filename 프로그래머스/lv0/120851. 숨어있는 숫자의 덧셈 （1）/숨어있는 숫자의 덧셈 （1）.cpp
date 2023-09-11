406749cd6b576da88d880f2b23ab691954c73539
LSBsity <126085673+LSBsity@users.noreply.github.com>
2023-09-12 02:43:08 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.01 ms, Memory: 3.54 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string)
+{
+    int answer = 0;
+
+    for (int i = 0; i < my_string.length(); i++)
+    {
+        if(isdigit(my_string[i]) != 0)
+            answer += (my_string[i] - 48);
+    }
+    
+    return answer;
+}
