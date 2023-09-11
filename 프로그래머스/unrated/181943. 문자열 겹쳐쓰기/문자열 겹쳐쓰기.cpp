d89b6833de791e203a30864740b28303bb733b55
LSBsity <126085673+LSBsity@users.noreply.github.com>
2023-09-12 03:14:17 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    my_string.replace(s, overwrite_string.size(), overwrite_string);
+    
+    return my_string;
+}
