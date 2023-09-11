3dcff36511395bf1c6a4e5a792a75d4753c68d4d
LSBsity <126085673+LSBsity@users.noreply.github.com>
2023-09-12 02:57:33 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(string my_string)
+{
+    vector<int> answer;
+
+    for (int i = 0; i < my_string.length(); i++)
+    {
+        if (my_string[i] >= 48 && my_string[i] <= 57)
+            answer.push_back(my_string[i] - 48);
+    }
+    
+    sort(answer.begin(), answer.end());
+
+    return answer;
+}
