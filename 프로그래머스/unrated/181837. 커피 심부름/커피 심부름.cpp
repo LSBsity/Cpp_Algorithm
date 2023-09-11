ed101334d1a32805792d643f1b6bb128de9e4495
LSBsity <126085673+LSBsity@users.noreply.github.com>
2023-09-12 03:09:01 +0900

    [unrated] Title: 커피 심부름, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<string> order) {
+    int answer = 0;
+    
+    for (int i = 0; i < order.size(); i++)
+    {
+        if (order[i].find("americano") != string::npos)
+            answer += 4500;
+        else if (order[i].find("cafelatte") != string::npos)
+            answer += 5000;
+        else
+            answer += 4500;
+    }
+    
+    
+    return answer;
+}
