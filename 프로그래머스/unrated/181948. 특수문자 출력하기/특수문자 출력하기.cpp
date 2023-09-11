8be2b3105ec9365f5ec4687eda164167b6b8557d
LSBsity <126085673+LSBsity@users.noreply.github.com>
2023-09-12 03:09:42 +0900

    [unrated] Title: 특수문자 출력하기, Time: 1.63 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    
+    //cout << "!@#$%^&*" << '('  << '\\' << "'" << "\"" << "<" << ">?:;";  
+    cout <<  R"(!@#$%^&*(\'"<>?:;)";
+
+    return 0;
+}
