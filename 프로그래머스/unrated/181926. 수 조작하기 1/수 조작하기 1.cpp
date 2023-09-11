236feda85bfb145bfb35a2d5fe6044aca9ab9bed
LSBsity <126085673+LSBsity@users.noreply.github.com>
2023-09-12 02:44:42 +0900

    [unrated] Title: 수 조작하기 1, Time: 0.17 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, string control) {
+    for (int i = 0; i < control.size(); i++)
+    {
+        switch(control[i])
+        {
+            case 'w':
+            n += 1;
+            break;
+            
+            case 's':
+            n -= 1;
+            break;
+                
+            case 'd':
+            n += 10;
+            break;
+                
+            case 'a':
+            n -= 10;
+            break;
+        }
+    }
+    
+    return n;
+}
