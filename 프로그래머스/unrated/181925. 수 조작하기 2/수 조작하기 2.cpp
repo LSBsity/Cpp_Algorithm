41081ba0d7096e3be45d35c230728c775e213818
LSBsity <126085673+LSBsity@users.noreply.github.com>
2023-09-12 03:01:55 +0900

    [unrated] Title: 수 조작하기 2, Time: 0.83 ms, Memory: 5.91 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> numLog) {
+    string answer = "";
+    
+    for (int i = 1; i < numLog.size(); i++)
+    {
+        if (numLog[i] == numLog[i - 1] + 1)
+            answer += 'w';
+        else if (numLog[i] == numLog[i - 1] -1)
+            answer += 's';
+        else if (numLog[i] == numLog[i - 1] + 10)
+            answer += 'd';
+        else
+            answer += 'a';
+    }
+    
+    return answer;
+}
