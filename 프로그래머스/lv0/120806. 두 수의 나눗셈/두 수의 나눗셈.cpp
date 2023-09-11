c74fd98ffcd231a79b7e6624cf4ff9b3e156da24
LSBsity <126085673+LSBsity@users.noreply.github.com>
2023-09-12 02:37:11 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 3.62 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    double a = num1;
+    double b = num2;
+    int ans = (a / b) * 1000;
+    return ans;
+}
