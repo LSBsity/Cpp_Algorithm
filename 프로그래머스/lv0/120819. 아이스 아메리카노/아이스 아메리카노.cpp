cc625922ddbc9e5ca7348c8ac00b209a8906f0de
LSBsity <126085673+LSBsity@users.noreply.github.com>
2023-09-12 02:38:35 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int money) {
+    vector<int> answer;
+    answer.push_back(money / 5500);
+    answer.push_back(money % 5500);
+
+    return answer;
+}
