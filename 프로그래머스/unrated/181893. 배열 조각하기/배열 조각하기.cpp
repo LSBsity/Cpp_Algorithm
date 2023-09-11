ef6a024143736bce5a72170c8b09af1a6d484229
LSBsity <126085673+LSBsity@users.noreply.github.com>
2023-09-12 03:18:03 +0900

    [unrated] Title: 배열 조각하기, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<int> query) {
+    for (int i = 0; i < query.size(); i++)
+    {
+        if (i % 2)
+            arr.erase(arr.begin(), arr.begin() + query[i]);
+        else
+            arr.erase(arr.begin() + query[i] + 1, arr.end());
+    }
+    return arr;
+}
