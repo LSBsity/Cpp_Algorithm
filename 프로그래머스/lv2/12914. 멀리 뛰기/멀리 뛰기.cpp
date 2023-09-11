f870e5be1751a59ede7c5b5806c2ce3e1ce710e6
LSBsity <126085673+LSBsity@users.noreply.github.com>
2023-09-12 03:40:40 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.03 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n)
+{
+    vector<long long> DP(n + 1, 0);
+    DP[1] = 1;
+    DP[2] = 2;
+    for (int i = 3; i <= n; i++)
+    {
+        DP[i] = DP[i - 1] + DP[i - 2];
+        DP[i] = DP[i] % 1234567;
+    }
+    
+    return DP.back() % 1234567;
+}
