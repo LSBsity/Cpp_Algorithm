d8babb97708cc4bef573b0bd62452912609acb64
SITY <126085673+LSBsity@users.noreply.github.com>
2023-09-19 10:55:37 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int x, y;
+
+    while (1)
+    {
+        cin >> x >> y;
+
+        if (x == 0 && y == 0)
+            break;
+        if (y % x == 0)
+            cout << "factor" << '\n';
+        else if (x % y == 0)
+            cout << "multiple" << '\n';
+        else
+        {
+            cout << "neither" << '\n';
+        }
+    }
+
+    return 0;
+}
