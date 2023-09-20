e2a42bdf5e863037208ff790b1da201a303669b4
SITY <126085673+LSBsity@users.noreply.github.com>
2023-09-20 17:49:29 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s = "         ,r'\"7\n";
+           s += "r`-_   ,'  ,/\n";
+           s += " \\. \". L_r'\n";
+           s += "   `~\\/\n";
+           s += "      |\n";
+           s += "      |";
+
+    cout << s;
+    return 0;
+}
