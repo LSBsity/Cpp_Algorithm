92fd581e0de66fca3da2cf8f828193e081417afa
SITY <126085673+LSBsity@users.noreply.github.com>
2023-09-20 17:49:17 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 124 ms, Memory: 10896 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+#define MAX 100001
+
+vector<int> graph[MAX];
+bool visited[MAX];
+int result[MAX];
+int cnt = 0;
+
+bool compare(int a, int b)
+{
+    return a > b;
+}
+
+void dfs(int r)
+{
+    visited[r] = true;
+    cnt++;
+    result[r] = cnt;
+    for (int i = 0; i < graph[r].size(); i++)
+    {
+        int y = graph[r][i];
+        if (!visited[y])
+        {
+            dfs(y);
+        }
+    }
+}
+
+
+
+
+
+int main()
+{
+    int n, m, r;
+    scanf("%d %d %d", &n, &m, &r);
+
+    for (int i = 1; i <= m; i++)
+    {
+        int n, m;
+        scanf("%d %d", &n, &m);
+        graph[n].push_back(m);
+        graph[m].push_back(n);
+    }
+    for (int i = 1; i <= n; i++)
+        sort(graph[i].begin(), graph[i].end(), compare);
+
+    dfs(r);
+
+    for (int i = 1; i <= n; i++)
+        cout << result[i] << '\n';
+    
+    
+
+    return 0;
+}
