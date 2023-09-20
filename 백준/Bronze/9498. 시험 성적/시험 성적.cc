1ce92fe2a97cdc225e7ce1dc8417242c2281aff6
SITY <126085673+LSBsity@users.noreply.github.com>
2023-09-20 17:29:00 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cin >> a;
+
+    if (a >= 90)
+        cout << "A" << endl;
+    else if (a >= 80 && a <= 89)
+        cout << "B" << endl;
+    else if (a >= 70 && a <= 79)
+        cout << "C" << endl;
+    else if (a >= 60 && a <= 69)
+        cout << "D" << endl;
+    else   
+        cout << "F" << endl;
+
+    return 0;
+}
