3e54363ff123459a5b8f7f0f804f7b8152f697fc
SITY <126085673+LSBsity@users.noreply.github.com>
2023-09-20 17:34:06 +0900

    [Bronze V] Title: A+B - 4, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    while (!(cin >> a >> b).eof())
+    {
+        cout << a + b << endl;
+    }
+
+
+    return 0;
+}
