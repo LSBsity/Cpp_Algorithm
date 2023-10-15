b67fcecbde16daaa1672938ebba9b27bef03a73a
SITY <126085673+LSBsity@users.noreply.github.com>
2023-10-16 00:00:30 +0900

    [level 2] Title: 더 맵게, Time: 213.42 ms, Memory: 40.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(vector<int> v, int k) {
+    int answer = 0;
+
+    priority_queue<int, vector<int>, greater<int>> q(v.begin(), v.end());
+
+    while (q.top() < k && q.size() >= 2) {
+        int i = q.top();
+        q.pop();
+        int j = q.top();
+        q.pop();
+
+        q.push(i + j * 2);
+        answer++;
+    }
+
+    return q.size() < 2 && q.top() < k ? -1 : answer;
+}
