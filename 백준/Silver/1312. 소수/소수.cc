c81b602f5d3d3cd1566efe34d05f94028a833be4
SITY <126085673+LSBsity@users.noreply.github.com>
2023-11-24 17:39:05 +0900

    [Silver V] Title: 소수, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1312. 소수/소수.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,n;
+    cin >> a >> b >> n;
+    for(int i = 1 ; i <= n ; i++){
+        a %= b;
+        a *= 10;
+    }
+    cout << a/b;
+    return 0;
+}
