7e1dc515fe673eb9faa41f2c774a6290d7340a4d
SITY <126085673+LSBsity@users.noreply.github.com>
2023-12-03 22:37:41 +0900

    [Bronze III] Title: 도미노, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2921. 도미노/도미노.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	int sum = 0;
+	cin >> n;
+
+	for (int i = 0; i < n+1; i++) {
+		for (int j = 0; j < i+1; j++) {
+			sum += i + j;
+		}
+	}
+	cout << sum;
+
+	return 0;
+}
