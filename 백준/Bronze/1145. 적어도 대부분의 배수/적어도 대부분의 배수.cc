916672da217e292d2b2100cfdfea7400803465a1
SITY <126085673+LSBsity@users.noreply.github.com>
2023-12-04 16:08:21 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int a[5] = {0}; 
+int num = 1;
+int main() {
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    while (1) {
+        int cnt = 0;
+        for (int i = 0; i < 5; i++) {
+            if (num % a[i] == 0) {
+                cnt++;
+            }
+        }
+        if (cnt >= 3) {
+            printf("%d", num);
+            break;
+        }
+        num++;
+    }
+
+    return 0;
+}
