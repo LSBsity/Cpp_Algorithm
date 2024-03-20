86b50cfd940cbbb0ea343b708eed8953732dde58
SITY <126085673+LSBsity@users.noreply.github.com>
2024-03-20 23:33:16 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.09 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include <iostream>
+
+using namespace std;
+
+int getNum(int n)
+{
+    int cnt = 0;
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            cnt++;
+            if (i * i < n)
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
+int solution(int number, int limit, int power)
+{
+    int answer = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        int result = getNum(i);
+        if (result > limit)
+            answer += power;
+        else
+            answer += result;
+    }
+    return answer;
+}
