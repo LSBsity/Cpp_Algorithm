89b0282ace563f46bca9d70cd055443cee1b574e
SITY <126085673+LSBsity@users.noreply.github.com>
2024-10-12 02:53:47 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 9.19 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        return Arrays.stream(num_list)
+                .sorted()
+                .limit(5)
+                .toArray();
+    }
+}
