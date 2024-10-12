94f15897492d7c1073fe9ca0cd55371575bd79ae
SITY <126085673+LSBsity@users.noreply.github.com>
2024-10-13 01:49:53 +0900

    [level 0] Title: 약수 구하기, Time: 5.37 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,9 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int n) {
+        return IntStream.rangeClosed(1, n)
+                .filter(i -> n % i == 0)
+                .toArray();
+    }
+}
