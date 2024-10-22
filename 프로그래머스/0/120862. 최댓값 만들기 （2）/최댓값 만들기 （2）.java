9fc6f18dd2fe2db8990e409a0f0f3cdf4a0c874d
SITY <126085673+LSBsity@users.noreply.github.com>
2024-10-22 22:06:33 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 3.11 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -2,10 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] numbers) {
-        if (numbers.length == 2) return numbers[0] * numbers[1];
         int[] arr = Arrays.stream(numbers).sorted().toArray();
-        int i = arr[0] * arr[1];
-        int j = arr[numbers.length - 2] * arr[numbers.length - 1];
-        return Math.max(i, j);
+        return Math.max(arr[0] * arr[1], arr[numbers.length - 2] * arr[numbers.length - 1]);
     }
 }
