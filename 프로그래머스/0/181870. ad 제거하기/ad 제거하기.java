5227a5bdc9cdee0871b22b97ea239403eae7d33d
SITY <126085673+LSBsity@users.noreply.github.com>
2024-11-15 11:35:43 +0900

    [level 0] Title: ad 제거하기, Time: 3.93 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        return Arrays.stream(strArr)
+                .filter(str -> !str.contains("ad"))
+                .toArray(String[]::new);
+    }
+}
