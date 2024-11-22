14e9a03df1cda61a57bdbe7c7c21571bf72378f7
SITY <126085673+LSBsity@users.noreply.github.com>
2024-11-22 10:50:19 +0900

    [level 1] Title: 하샤드 수, Time: 2.16 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = String.valueOf(x)
+                .chars()
+                .map(c -> c - '0')
+                .sum();
+        return x % sum == 0;
+    }
+}
