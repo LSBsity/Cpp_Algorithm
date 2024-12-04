afd3accdcdbbdf4603b4a2b7867b167ef9a07f35
SITY <126085673+LSBsity@users.noreply.github.com>
2024-12-04 11:16:23 +0900

    [level 0] Title: 날짜 비교하기, Time: 1.16 ms, Memory: 94.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+class Solution {
+    public int solution(int[] date1, int[] date2) {       
+        LocalDate d1 = LocalDate.of(date1[0], date1[1], date1[2]);
+        LocalDate d2 = LocalDate.of(date2[0], date2[1], date2[2]);
+        
+        return d1.isBefore(d2) ? 1 : 0;
+    }
+}
