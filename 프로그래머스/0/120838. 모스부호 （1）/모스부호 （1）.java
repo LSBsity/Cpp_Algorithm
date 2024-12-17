9c4df6e721640a93fff961964b60976ea6861200
SITY <126085673+LSBsity@users.noreply.github.com>
2024-12-18 02:47:41 +0900

    [level 0] Title: 모스부호 (1), Time: 1.99 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class Solution {
+
+    private final Map<String, String> morse;
+
+    public Solution() {
+        morse = new HashMap<>();
+        morse.put(".-", "a");
+        morse.put("-...", "b");
+        morse.put("-.-.", "c");
+        morse.put("-..", "d");
+        morse.put(".", "e");
+        morse.put("..-.", "f");
+        morse.put("--.", "g");
+        morse.put("....", "h");
+        morse.put("..", "i");
+        morse.put(".---", "j");
+        morse.put("-.-", "k");
+        morse.put(".-..", "l");
+        morse.put("--", "m");
+        morse.put("-.", "n");
+        morse.put("---", "o");
+        morse.put(".--.", "p");
+        morse.put("--.-", "q");
+        morse.put(".-.", "r");
+        morse.put("...", "s");
+        morse.put("-", "t");
+        morse.put("..-", "u");
+        morse.put("...-", "v");
+        morse.put(".--", "w");
+        morse.put("-..-", "x");
+        morse.put("-.--", "y");
+        morse.put("--..", "z");
+    }
+
+    public String solution(String letter) {
+        return Arrays.stream(letter.split(" "))
+                .map(morse::get)
+                .collect(Collectors.joining());
+    }
+}
