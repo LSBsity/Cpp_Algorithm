b1b3c7b393cdcfb7706d9a8d11c6acd35deaf019
SITY <126085673+LSBsity@users.noreply.github.com>
2025-01-28 14:15:40 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.08 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,21 +1,13 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        String str = "";
-
+        StringBuilder sb = new StringBuilder();
+        
         while (n > 0) {
-            int remain = n % 3;
-            str += remain;
+            sb.append(n % 3);
             n /= 3;
         }
-        
-        for (int i = str.length() - 1, exp = 0; i >= 0; i--) {
-            char c = str.charAt(i);
-            answer += ((int) c - '0') * Math.pow(3, exp++);
-        }
-        
-        
-        return answer;
+
+        return Integer.parseInt(sb.toString(), 3);
     }
 }
 
