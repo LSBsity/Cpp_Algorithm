2086175b4f2676f26ab56cff001eb511dfbce711
SITY <126085673+LSBsity@users.noreply.github.com>
2025-01-31 18:54:02 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.06 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,22 +1,22 @@
 import java.util.Set;
 import java.util.HashSet;
+
 class Solution {
 
     private final Set<String> set = new HashSet<>();
 
     public int[] solution(int n, String[] words) {
-        String lastWord = words[0];
-        set.add(lastWord);
+        String last = words[0];
+        set.add(last);
+
         for (int i = 1; i < words.length; i++) {
-            if ((lastWord.charAt(lastWord.length() - 1) != words[i].charAt(0)) || set.contains(
-                    words[i])) {
+            if (last.charAt(last.length() - 1) != words[i].charAt(0) || !set.add(words[i])) {
                 return new int[]{(i % n) + 1, (i / n) + 1};
             }
 
-            set.add(words[i]);
-            lastWord = words[i];
+            last = words[i];
         }
+
         return new int[]{0, 0};
     }
 }
-
