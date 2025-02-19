e9fd269d8b41d59003ba5e5e6e1793040df0cce3
SITY <126085673+LSBsity@users.noreply.github.com>
2025-02-19 16:16:58 +0900

    [level 3] Title: 기지국 설치, Time: 0.64 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,30 +1,43 @@
 class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        int left = 1;
+
         int cover = w * 2 + 1;
+        int right = 1;
+        for (int i = 0; i < stations.length; i++) {
+            int station = stations[i];
+            int start = station - w;
+            int end = station + w;
 
-        for (int station : stations) {
-            int distance = station - w - left;
-
+            int distance = start - right;
+            right = end + 1;
             if (distance > 0) {
-                answer += distance / cover;
-                if (distance % cover != 0) {
+                if (distance <= cover) {
                     answer++;
+                } else {
+                    answer += distance / cover;
+                    if (distance % cover != 0) {
+                        answer++;
+                    }
+                }
+
+            }
+        }
+
+        if (right <= n) {
+            int distance = n - right + 1;
+            if (distance > 0) {
+                if (distance <= cover) {
+                    answer++;
+                } else {
+                    answer += distance / cover;
+                    if (distance % cover != 0) {
+                        answer++;
+                    }
                 }
             }
 
-            left = station + w + 1;
         }
-
-        if (left <= n) {
-            int distance = n - left + 1;
-            answer += distance / cover;
-            if (distance % cover != 0) {
-                answer++;
-            }
-        }
-
         return answer;
     }
 }
