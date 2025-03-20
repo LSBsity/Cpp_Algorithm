1deb2b76d66eeb98744ce9b5ce92b3c165f4d860
SITY <126085673+LSBsity@users.noreply.github.com>
2025-03-20 22:43:33 +0900

    [level 2] Title: 롤케이크 자르기, Time: 235.01 ms, Memory: 175 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,28 +1,25 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] topping) {
+    public int solution(int[] arr) {
         int answer = 0;
         
         Map<Integer, Integer> map = new HashMap<>();
         Set<Integer> set = new HashSet<>();
-
-        for (int i : topping) {
+        
+        for (var i : arr) {
             map.put(i, map.getOrDefault(i, 0) + 1);
         }
-
-        for (int i : topping) {
+        
+        for (var i : arr) {
             set.add(i);
             map.put(i, map.get(i) - 1);
-
-            if (map.get(i) == 0) {
-                map.remove(i);
-            }
-            if (set.size() == map.size()) {
-                answer++;
-            }
+            
+            if (map.get(i) == 0) map.remove(i);
+            if (set.size() == map.size()) answer++;
         }
-
+        
+        
         return answer;
     }
 }
