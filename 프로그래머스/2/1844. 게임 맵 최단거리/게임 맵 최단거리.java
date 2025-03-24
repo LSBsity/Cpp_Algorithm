1fe7959b793887c2380a11ef73cad8eec002f585
SITY <126085673+LSBsity@users.noreply.github.com>
2025-03-24 13:47:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.14 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,47 +1,49 @@
 import java.util.*;
 
 class Solution {
+    
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, 1, -1};
+    
     static class Node {
-        int x, y, dist;
-        Node(int x, int y, int dist) {
+        int x;
+        int y;
+        int dist;
+        
+        public Node(int x, int y, int dist) {
             this.x = x;
             this.y = y;
             this.dist = dist;
         }
     }
-
+    
     public int solution(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
         
         boolean[][] visited = new boolean[n][m];
-        Queue<Node> queue = new LinkedList<>();
-        
-        queue.add(new Node(0, 0, 1));
-        visited[0][0] = true;
-        
-        while (!queue.isEmpty()) {
-            Node node = queue.poll();
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(0, 0, 1));
+        while (!q.isEmpty()) {
+            Node node = q.poll();
             int x = node.x;
             int y = node.y;
             int dist = node.dist;
             
-            if (x == n - 1 && y == m - 1) {
-                return dist;
-            }
+            if (x == n - 1 && y == m - 1) return dist;
             
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+                int nx = x + this.dx[i];
+                int ny = y + this.dy[i];
                 
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] == 1 && !visited[nx][ny]) {
-                    queue.add(new Node(nx, ny, dist + 1));
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && maps[nx][ny] == 1) {
                     visited[nx][ny] = true;
+                    q.add(new Node(nx, ny, dist + 1));
                 }
-            }
+            } 
         }
+        
+        
         return -1;
     }
 }
