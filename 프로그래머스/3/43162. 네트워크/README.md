2a21b7d7632ba4e04389162603252e764464faf8
SITY <126085673+LSBsity@users.noreply.github.com>
2025-04-21 14:37:48 +0900

    [level 3] Title: 네트워크, Time: 0.15 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162#) 
 
 ### 성능 요약
 
-메모리: 77.9 MB, 시간: 0.18 ms
+메모리: 88.8 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 21일 14:29:11
+2025년 04월 21일 14:37:46
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,25 +1,21 @@
-import java.util.*;
 class Solution {
-    public int solution(int n, int[][] computers) {
-        boolean[] visited = new boolean[n];
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(computers, visited, i);
-                count++;
+    public int solution(int n, int[][] arr) {
+        int cnt = 0;
+        boolean[] v = new boolean[n];
+        for (int i = 0; i < arr.length; i++) {
+            if (!v[i]) {
+                this.dfs(arr, v, i);
+                cnt++;
             }
         }
-
-        return count;
+        return cnt;
     }
-
-    private void dfs(int[][] computers, boolean[] visited, int start) {
-        visited[start] = true;
-        
-        for (int i = 0; i < computers.length; i++) {
-            if (computers[start][i] == 1 && !visited[i]) {
-                dfs(computers, visited, i);
+    
+    public void dfs(int[][] arr, boolean[] v, int s) {
+        v[s] = true;
+        for (int i = 0; i < arr.length; i++) {
+            if (!v[i] && arr[s][i] == 1) {
+                this.dfs(arr, v, i);
             }
         }
     }
