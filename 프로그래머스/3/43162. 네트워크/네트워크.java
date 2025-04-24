218bece11b529a683b387522f5582e4056bb0949
SITY <126085673+LSBsity@users.noreply.github.com>
2025-04-24 12:10:02 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,25 +1,25 @@
 class Solution {
     
-    private void dfs(int[][] arr, boolean[] v, int s) {
-        v[s] = true;
-        for (int i = 0; i < arr.length; i++) {
-            if (!v[i] && arr[s][i] == 1) {
-                this.dfs(arr, v, i);
+    private void go(int[][] computers, boolean[] v, int node) {
+        v[node] = true;
+        for (int i = 0; i < computers[0].length; i++) {
+            if (!v[i] && computers[node][i] == 1) {
+                this.go(computers, v, i);
             }
         }
     }
-
+    
     public int solution(int n, int[][] computers) {
-        int count = 0;
-        boolean[] v = new boolean[n];
+        int answer = 0;
         
-        for (int i = 0; i < computers.length; i++) {
+        boolean[] v = new boolean[n];
+        for (int i = 0; i < n; i++) {
             if (!v[i]) {
-                this.dfs(computers, v, i);
-                count++;
+                this.go(computers, v, i);
+                answer++;
             }
         }
         
-        return count;
+        return answer;
     }
 }
