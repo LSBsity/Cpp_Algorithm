234da4a7c60637cf3e7e7de2c76851943f6115df
SITY <126085673+LSBsity@users.noreply.github.com>
2025-04-25 12:02:55 +0900

    [level 3] Title: 정수 삼각형, Time: 8.91 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] arr) {
-        for (int i = arr[arr.length - 1].length - 1 - 1; i >= 0; i--) {
+        for (int i = arr.length - 2; i >= 0; i--) {
             for (int j = arr[i].length - 1; j >= 0; j--) {
                 arr[i][j] = Math.max(arr[i][j] + arr[i + 1][j], arr[i][j] + arr[i + 1][j + 1]);
             }
