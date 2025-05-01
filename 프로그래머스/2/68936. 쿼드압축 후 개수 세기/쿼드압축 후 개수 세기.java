ea9f94f9bf80c2f62280a61172a63d39a8b96a3c
SITY <126085673+LSBsity@users.noreply.github.com>
2025-05-01 14:38:11 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 19.62 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,26 +1,27 @@
-import java.util.*;
-
 class Solution {
-
-    int[] answer = new int[]{0, 0};
-
-    public int go(int[][] arr, int row, int col, int size) {
+    
+    int[] answer = {0, 0};
+    
+    private int go(int[][] arr, int row, int col, int size) {
         if (size == 1) {
             answer[arr[row][col]]++;
             return arr[row][col];
         }
+        
         int half = size / 2;
-        int a = go(arr, row, col, half);
-        int b = go(arr, row, col + half, half);
-        int c = go(arr, row + half, col, half);
-        int d = go(arr, row + half, col + half, half);
-        if (a == b && b == c && c == d && a != Integer.MIN_VALUE) {
+        int a = this.go(arr, row, col, size / 2);
+        int b = this.go(arr, row, col + half, size / 2);
+        int c = this.go(arr, row + half, col, size / 2);
+        int d = this.go(arr, row + half, col + half, size / 2);
+        
+        if (a == b & b == c & c == d && a != -1) {
             answer[a] -= 3;
             return a;
         }
-        return Integer.MIN_VALUE;
+    
+        return -1;
     }
-
+    
     public int[] solution(int[][] arr) {
         this.go(arr, 0, 0, arr.length);
         return answer;
