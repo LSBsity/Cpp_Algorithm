eaeb9656679e7ef0437d338d31238d707afa1163
SITY <126085673+LSBsity@users.noreply.github.com>
2025-05-03 13:57:32 +0900

    [level 2] Title: 숫자 변환하기, Time: 69.83 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -4,28 +4,24 @@ class Solution {
     public int solution(int x, int y, int n) {
         Queue<Integer> q = new LinkedList<>();
         boolean[] v = new boolean[1_000_001];
-        
         q.offer(x);
         v[x] = true;
-        int level = 0;
-        
+        int cnt = 0;
         while (!q.isEmpty()) {
             int size = q.size();
             for (int i = 0; i < size; i++) {
-                int current = q.poll();
-                if (current == y) return level;
-                
-                int[] nexts = {current + n, current * 2, current * 3};
-                for (int next : nexts) {
+                int cur = q.poll();
+                if (cur == y) return cnt;
+                int[] nexts = {cur + n, cur * 2, cur * 3};
+                for (var next : nexts) {
                     if (next <= y && !v[next]) {
                         v[next] = true;
                         q.offer(next);
                     }
                 }
             }
-            level++;
+            cnt++;    
         }
-        
         return -1;
     }
 }
