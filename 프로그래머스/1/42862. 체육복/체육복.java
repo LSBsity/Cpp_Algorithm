f01696d4efe9362c5a3f7f85bac0578e4ddd808b
SITY <126085673+LSBsity@users.noreply.github.com>
2025-05-07 11:03:22 +0900

    [level 1] Title: 체육복, Time: 15.10 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,37 +2,35 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int losts = n - lost.length; 
+        int saved = n - lost.length;
+        boolean[] v = new boolean[31];
         
         Arrays.sort(reserve);
         Arrays.sort(lost);
         
-        boolean[] v = new boolean[31];
-        for (var i : reserve) { 
+        for (var i : reserve) {
             for (var j : lost) {
                 if (i == j) {
-                    losts++;
                     v[i] = true;
+                    saved++;
                     break;
                 }
             }
         }
-
-        for (int i = 0, idx = 0; i < reserve.length; i++) {
-            if (idx >= lost.length) break;
-            if (v[reserve[i]]) continue;
-            
-            for (int j = idx; j < lost.length; j++) {
-                if (v[lost[j]]) continue;
-                if (Math.abs(reserve[i] - lost[j]) == 1) {
-                    v[lost[j]] = true;
-                    idx++;
-                    losts++;
+        
+        for (var i : reserve) {
+            if (v[i]) {
+                continue;
+            }
+            for (var j : lost) {
+                if (!v[j] && Math.abs(j - i) == 1) {
+                    v[j] = true;
+                    saved++;
                     break;
-                } 
+                }
             }
         }
         
-        return losts;
+        return saved;
     }
 }
