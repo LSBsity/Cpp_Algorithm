83fd44e0a7512c9d3f08f0e865ea86fe768c9fa5
SITY <126085673+LSBsity@users.noreply.github.com>
2025-05-10 12:10:56 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 9.33 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,60 +1,58 @@
 class Solution {
     public int solution(int m, int n, String[] board) {
         int answer = 0;
+        
         int[][] arr = new int[m][n];
 
         for (int i = 0; i < m; i++) {
             char[] chars = board[i].toCharArray();
             for (int j = 0; j < n; j++) {
-                arr[i][j] = chars[j] - 'A';    
+                arr[i][j] = chars[j];
             }
         }
         
         while (true) {
             boolean[][] v = new boolean[m][n];
-            boolean erased = false;
+            boolean check = false;
+            
             for (int i = 0; i < m - 1; i++) {
                 for (int j = 0; j < n - 1; j++) {
-                    if (arr[i][j] == -1) continue;
-                    if (arr[i][j] == arr[i][j + 1] && 
-                        arr[i][j + 1] == arr[i + 1][j + 1] && 
-                        arr[i + 1][j + 1] == arr[i + 1][j]) {
-                        v[i][j] = v[i][j + 1] = v[i + 1][j] = v[i + 1][j + 1] = true;
-                        erased = true;
+                    if (arr[i][j] == arr[i][j + 1] &&
+                        arr[i][j + 1] == arr[i + 1][j + 1] &&
+                        arr[i + 1][j + 1] == arr[i + 1][j] &&
+                        arr[i][j] != -1) {
+                        v[i][j] = v[i][j + 1] = v[i + 1][j + 1] = v[i + 1][j] = true;
+                        check = true;
                     }
                 }
-            }
-            
-            if (!erased) {
-                break;
             }
 
+            if (!check) {
+                for (int i = 0; i < m; i++) { 
+                    for (int j = 0; j < n; j++) { 
+                        if (arr[i][j] == -1) answer++;
+                    }
+                }
+                return answer;
+            }
+            
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (v[i][j]) {
-                        arr[i][j] = -1;
-                    }
+                    if (v[i][j]) arr[i][j] = -1;
                 }
             }
             
             for (int j = 0; j < n; j++) {
-                int empty = m - 1;
+                int emptyRow = m - 1;
                 for (int i = m - 1; i >= 0; i--) {
                     if (arr[i][j] != -1) {
-                        arr[empty][j] = arr[i][j];
-                        if (empty != i) arr[i][j] = -1;
-                        empty--;
+                        arr[emptyRow][j] = arr[i][j];
+                        if (emptyRow != i) arr[i][j] = -1;
+                        emptyRow--;
                     }
                 }
             }
         }
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (arr[i][j] == -1) answer++;
-            }
-        }
-        
-        return answer;
     }
+    
 }
