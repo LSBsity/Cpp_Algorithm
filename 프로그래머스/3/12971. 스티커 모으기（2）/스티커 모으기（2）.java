66adc9053a8352f101a2b1f76e9dd0820bfbf52b
SITY <126085673+LSBsity@users.noreply.github.com>
2025-05-18 09:53:47 +0900

    [level 3] Title: 스티커 모으기(2), Time: 7.27 ms, Memory: 59.9 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,13 +1,14 @@
+import java.util.*;
 class Solution {
     public int solution(int sticker[]) {
         if (sticker.length == 1) return sticker[0];
         
         int size = sticker.length;
-        
+
         int[] dp1 = new int[size];
         dp1[0] = sticker[0];
         dp1[1] = sticker[0];
-        for (int i = 2; i < size; i++) {
+        for (int i = 2; i < size - 1; i++) {
             dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + sticker[i]);
         }
         
@@ -16,8 +17,8 @@ class Solution {
         dp2[1] = sticker[1];
         for (int i = 2; i < size; i++) {
             dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + sticker[i]);
-        }       
-        
+        }
+
         return Math.max(dp1[size - 2], dp2[size - 1]);
     }
 }
