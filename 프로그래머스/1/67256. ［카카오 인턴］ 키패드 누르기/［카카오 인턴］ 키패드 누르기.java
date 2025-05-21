cb59458c683f469df19a84e6752108a8d1f8057b
SITY <126085673+LSBsity@users.noreply.github.com>
2025-05-21 11:44:53 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 2.35 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -2,106 +2,93 @@ import java.util.*;
 
 class Solution {
     
-    int[] dx = {-1, 0, 1, 0};
-    int[] dy = {0, 1, 0, -1};
     int[][] map = {
         {1, 2, 3},
         {4, 5, 6},
         {7, 8, 9},
-        {-1, 0, -2}
+        {10, 0, 12}
+    };
+    int[][] coord = {
+        {3, 1},
+        {0, 0},
+        {0, 1},
+        {0, 2},
+        {1, 0},
+        {1, 1},
+        {1, 2},
+        {2, 0},
+        {2, 1},
+        {2, 2},
+        {3, 0},
+        {3, 1},
+        {3, 2}
     };
     
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
+    
     public String solution(int[] numbers, String hand) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         
-        int leftCur = -1, rightCur = -2;
+        int left = 10;
+        int right = 12;
         for (var i : numbers) {
-            char c;
-            if (i == 1 || i == 4 || i == 7) {
-                leftCur = i;
-                c = 'L';
-            } else if (i == 3 || i == 6 || i == 9) {
-                rightCur = i;
-                c = 'R';
+            if (i % 3 == 1) {
+                sb.append('L');
+                left = i;
+            } else if (i != 0 && i % 3 == 0) {
+                sb.append('R');
+                right = i;
             } else {
-                int left = this.getCloser(leftCur, i);
-                int right = this.getCloser(rightCur, i);
-                if (left > right) {
-                    rightCur = i;
-                    c = 'R';
-                } else if (left < right) {
-                    leftCur = i;
-                    c = 'L';
+                int leftDist = this.go(left, i);
+                int rightDist = this.go(right, i);
+                char dir = ' ';
+                if (leftDist > rightDist) {
+                    right = i;
+                    dir = 'R';
+                } else if (leftDist < rightDist) {
+                    left = i;
+                    dir = 'L';
                 } else {
-                    if (hand.equals("left")) {
-                        leftCur = i;
-                        c = 'L';
-                    } else {
-                        rightCur = i;
-                        c = 'R';
+                    if (hand.equals("right")) {
+                        right = i;
+                        dir = 'R';
+                    } else if (hand.equals("left")) {
+                        left = i;
+                        dir = 'L';
                     }
                 }
+                sb.append(dir);
             }
-            answer += c;
         }
         
-        return answer;
+        return sb.toString();
     }
     
-    private int getCloser(int current, int target) {
-        for (int i = 0; i < 4; i++) {
-             for (int j = 0; j < 3; j++) {
-                 if (map[i][j] == current) {
-                     return this.bfs(i, j, target);
-                 }
-             }
-        }
-        return -1;
-    }
-    
-    private int bfs(int x, int y, int target) {
-        Queue<Element> q = new LinkedList<>();
-        q.offer(new Element(x, y, 0));
-        
+    private int go(int current, int target) {
         boolean[][] v = new boolean[4][3];
+        int[] xy = coord[current];
+        Queue<int[]> q = new LinkedList<int[]>();
+        q.offer(new int[]{xy[0], xy[1], 0});
+        v[xy[0]][xy[1]] = true;
         
-        while(!q.isEmpty()) {
-            Element cur = q.poll();
-            int curLevel = cur.level;
-            if (map[cur.xy[0]][cur.xy[1]] == target) {
-                return curLevel;
+        
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            if (map[cur[0]][cur[1]] == target) {
+                return cur[2];
             }
+            
             for (int i = 0; i < 4; i++) {
-                int nx = cur.xy[0] + dx[i];
-                int ny = cur.xy[1] + dy[i];
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
                 if (nx >= 0 && nx < 4 && ny >= 0 && ny < 3 && !v[nx][ny]) {
+                    q.offer(new int[]{nx, ny, cur[2] + 1});
                     v[nx][ny] = true;
-                    q.offer(new Element(nx, ny, curLevel + 1));
                 }
             }
         }
+        
         return -1;
     }
-    
-    static class Element {
-        int[] xy = new int[2];
-        int level;
-        
-        public Element(int x, int y, int level) {
-            this.xy[0] = x;
-            this.xy[1] = y;
-            this.level = level;
-        }
-    }
 }
-
-/**
-1 -2
-1 3
-
-
-0 0 / 0 1 / 0 2
-1 0 / 1 1 / 1 2
-2 0 / 2 1 / 2 2
-- - / 3 1 / - -
-**/
