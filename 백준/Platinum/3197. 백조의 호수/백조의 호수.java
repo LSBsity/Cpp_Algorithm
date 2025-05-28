d42cc616d9a88de1469a3a95dffae2db850d59b9
SITY <126085673+LSBsity@users.noreply.github.com>
2025-05-28 14:14:55 +0900

    [Platinum V] Title: 백조의 호수, Time: 844 ms, Memory: 209796 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.java
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.java
@@ -2,56 +2,57 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static int r, c;
-    
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    static int[][] swans = new int[2][2];
-    
-    static char[][] map;
-    static Queue<int[]> meltQ = new LinkedList<>();
-    static Queue<int[]> swanQ = new LinkedList<>();
+    static int r;
+    static int c;
 
-    public static void main(String[] args) throws Exception {
+    static int[][] swan = new int[2][2];
+
+    static char[][] map;
+
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    static Queue<int[]> swanQ = new LinkedList<>();
+    static Queue<int[]> meltQ = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int answer = 0;
-
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
+
         map = new char[r][c];
-        boolean[][] swanVisited = new boolean[r][c];
+        boolean[][] visited = new boolean[r][c];
 
-        int idx = 0;
-
+        int swanIdx = 0;
         for (int i = 0; i < r; i++) {
-            String str = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < c; j++) {
-                map[i][j] = str.charAt(j);
+                map[i][j] = line.charAt(j);
                 if (map[i][j] == 'L') {
-                    swans[idx][0] = i;
-                    swans[idx++][1] = j;
+                    swan[swanIdx][0] = i;
+                    swan[swanIdx++][1] = j;
                 }
                 if (map[i][j] != 'X') {
-                    meltQ.add(new int[]{i, j});
+                    meltQ.offer(new int[]{i, j});
                 }
             }
         }
 
-        swanQ.add(new int[]{swans[0][0], swans[0][1]});
-        swanVisited[swans[0][0]][swans[0][1]] = true;
+        swanQ.offer(new int[]{swan[0][0], swan[0][1]});
+        visited[swan[0][0]][swan[0][1]] = true;
 
-        while (!canMeet(swanVisited)) {
+        int count = 0;
+        while (!canMeet(visited)) {
             melt();
-            answer++;
+            count++;
         }
-
+        System.out.println(count);
         br.close();
-        System.out.print(answer);
     }
 
-    private static boolean canMeet(boolean[][] swanVisited) {
-        Queue<int[]> nextQueue = new LinkedList<>();
+    private static boolean canMeet(boolean[][] visited) {
+        Queue<int[]> nextQ = new LinkedList<>();
 
         while (!swanQ.isEmpty()) {
             int x = swanQ.peek()[0];
@@ -62,37 +63,35 @@ class Main {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
-                if (nx < 0 || nx >= r || ny < 0 || ny >= c || swanVisited[nx][ny]) continue;
-                swanVisited[nx][ny] = true;
-                if (nx == swans[1][0] && ny == swans[1][1]) {
+                if (nx < 0 || nx >= r || ny < 0 || ny >= c || visited[nx][ny]) continue;
+
+                visited[nx][ny] = true;
+                if (nx == swan[1][0] && ny == swan[1][1]) {
                     return true;
                 } else if (map[nx][ny] == 'X') {
-                    nextQueue.add(new int[]{nx, ny});
+                    nextQ.offer(new int[]{nx, ny});
                 } else {
-                    swanQ.add(new int[]{nx, ny});
+                    swanQ.offer(new int[]{nx, ny});
                 }
             }
         }
-        swanQ = nextQueue;
+        swanQ = nextQ;
         return false;
     }
 
     private static void melt() {
         int size = meltQ.size();
-
         for (int i = 0; i < size; i++) {
             int x = meltQ.peek()[0];
             int y = meltQ.peek()[1];
             meltQ.poll();
-
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dx[k];
-                int ny = y + dy[k];
-
+            for (int j = 0; j < 4; j++) {
+                int nx = x + dx[j];
+                int ny = y + dy[j];
                 if (nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
                 if (map[nx][ny] == 'X') {
                     map[nx][ny] = '.';
-                    meltQ.add(new int[]{nx, ny});
+                    meltQ.offer(new int[]{nx, ny});
                 }
             }
         }
