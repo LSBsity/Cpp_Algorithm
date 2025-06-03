27dedd5ff15db39ef4b95d8223e01f853f23ca60
SITY <126085673+LSBsity@users.noreply.github.com>
2025-06-03 11:20:46 +0900

    [Silver V] Title: 올림픽, Time: 132 ms, Memory: 15084 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -11,26 +11,54 @@ class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        List<int[]> list = new ArrayList<>();
+        List<int[][]> list = new ArrayList<>();
         for (int t = 1; t <= N; t++) {
             st = new StringTokenizer(br.readLine());
             int num = Integer.parseInt(st.nextToken());
-            int val = 3 * Integer.parseInt(st.nextToken()) + 2 * Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
-            list.add(new int[]{num, val});
+            int gold = Integer.parseInt(st.nextToken());
+            int silver = Integer.parseInt(st.nextToken());
+            int bronze = Integer.parseInt(st.nextToken());
+            list.add(new int[][]{
+                    {num, 0, 0},
+                    {gold, silver, bronze}
+            });
         }
 
-        List<int[]> result = list.stream()
-                .sorted((o1, o2) -> Integer.compare(o2[1], o1[1]))
-                .collect(Collectors.toList());
-        
-        for (int i = 0, cnt = 0, last = -1; i < result.size(); i++) {
-            if (result.get(i)[1] != last) {
-                cnt++;
-                last = result.get(i)[1];
+        List<int[][]> result = list.stream()
+                .sorted((o1, o2) -> {
+                    if (o1[1][0] != o2[1][0]) {
+                        return o2[1][0] - o1[1][0];
+                    }
+                    if (o1[1][1] != o2[1][1]) {
+                        return o2[1][1] - o1[1][1];
+                    }
+                    return o2[1][2] - o1[1][2];
+                }).collect(Collectors.toList());
+
+        eval(result, K);
+    }
+
+    private static void eval(List<int[][]> result, int K) {
+        int cnt = 1;
+        int prevGold = -1, prevSilver = -1, prevBronze = -1;
+        int rank = 1;
+        for (int i = 0; i < result.size(); i++) {
+            int[][] current = result.get(i);
+            int num = current[0][0];
+            int gold = current[1][0];
+            int silver = current[1][1];
+            int bronze = current[1][2];
+            if (gold != prevGold || silver != prevSilver || bronze != prevBronze) {
+                rank = cnt;
+                prevGold = gold;
+                prevSilver = silver;
+                prevBronze = bronze;
             }
-            if (result.get(i)[0] == K) {
-                System.out.println(cnt);
+            if (num == K) {
+                System.out.println(rank);
+                break;
             }
+            cnt++;
         }
     }
 }
