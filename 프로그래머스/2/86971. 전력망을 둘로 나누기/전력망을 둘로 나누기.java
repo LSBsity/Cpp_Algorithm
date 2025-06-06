f4bfb2cead4ee721d208683085d3a6ae419aee4a
SITY <126085673+LSBsity@users.noreply.github.com>
2025-06-06 10:54:50 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.86 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,51 +1,45 @@
 class Solution {
     
-    private int getParent(int[] parent, int x) {
-        if (parent[x] == x) {
-            return x;
-        }
-        return parent[x] = getParent(parent, parent[x]);
+    private void unionParent(int[] parent, int a, int b) {
+        a = findParent(parent, a);
+        b = findParent(parent, b);
+        if (a > b) parent[a] = b;
+        else parent[b] = a;
     }
     
-    private void unionParent(int[] parent, int a, int b) {
-        a = getParent(parent, a);
-        b = getParent(parent, b);
-        if (a < b) {
-            parent[b] = a;
-        } else {
-            parent[a] = b;
-        }
+    private int findParent(int[] parent, int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = findParent(parent, parent[x]);
     }
     
     private int getGroupSize(int[] parent, int x) {
         int count = 0;
         for (int i = 1; i < parent.length; i++) {
-            if (getParent(parent, i) == x) {
+            if (findParent(parent, i) == x) {
                 count++;
             }
         }
         return count;
-     }
-
+    }
     
-    public int solution(int n, int[][] wires) {        
+    public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
         for (int i = 0; i < n; i++) {
-            int[] parent = new int[n + 1];    
+            int[] parent = new int[n + 1];
             for (int j = 1; j <= n; j++) {
                 parent[j] = j;
-            }
+            }    
             for (int j = 0; j < wires.length; j++) {
                 if (i == j) continue;
                 unionParent(parent, wires[j][0], wires[j][1]);
             }
             
-            int groupRoot = getParent(parent, 1);
-            int groupSize = getGroupSize(parent, groupRoot);
-            int size = n - groupSize;
-            int diff = Math.abs(groupSize - size);
+            int rootSize = getGroupSize(parent, 1);
+            int remain = n - rootSize;
+            int diff = Math.abs(remain - rootSize);
             answer = Math.min(answer, diff);
         }
+        
         return answer;
     }
 }
