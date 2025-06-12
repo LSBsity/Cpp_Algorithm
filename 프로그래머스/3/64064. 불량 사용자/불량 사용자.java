0ddf677da594b609605455be2bc0d520629dcb0c
SITY <126085673+LSBsity@users.noreply.github.com>
2025-06-12 13:16:06 +0900

    [level 3] Title: 불량 사용자, Time: 159.71 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,34 +1,34 @@
 import java.util.*;
 
 class Solution {
-    Set<Set<String>> result = new HashSet<>();
+    
+    Set<Set<String>> set = new HashSet<>();
     
     public int solution(String[] user_id, String[] banned_id) {
-        dfs(user_id, banned_id, 0, new HashSet<>());
-        return result.size();
+        go(user_id, banned_id, 0, new HashSet<>());
+        return set.size();
     }
     
-    private void dfs(String[] user_id, String[] banned_id, int idx, Set<String> current) {
+    private void go(String[] user_id, String[] banned_id, int idx, Set<String> current) {
         if (idx == banned_id.length) {
-            result.add(new HashSet<>(current));
+            set.add(new HashSet<>(current));
             return;
         }
         
-        for (String id : user_id) {
+        for (var id : user_id) {
             if (!current.contains(id) && isMatch(banned_id[idx], id)) {
                 current.add(id);
-                dfs(user_id, banned_id, idx + 1, current);
-                current.remove(id);
+                this.go(user_id, banned_id, idx + 1, current);
+                current.remove(id); 
             }
         }
     }
     
-    private boolean isMatch(String ban, String id) {
+    public boolean isMatch(String ban, String id) {
         if (ban.length() != id.length()) return false;
         for (int i = 0; i < ban.length(); i++) {
-            if (ban.charAt(i) != '*' && ban.charAt(i) != id.charAt(i)) {
-                return false;
-            }
+            char c = ban.charAt(i);
+            if (c != '*' && c != id.charAt(i)) return false;
         }
         return true;
     }
