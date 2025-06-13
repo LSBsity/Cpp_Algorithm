edaf291638410763d85c89f843d2228c6d4afded
SITY <126085673+LSBsity@users.noreply.github.com>
2025-06-13 14:30:52 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.28 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int[] answer = {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE};
+        for (int i = 0; i < wallpaper.length; i++) {
+            for (int j = 0; j < wallpaper[0].length(); j++) {
+                if (wallpaper[i].charAt(j) == '#') {
+                    answer[0] = Math.min(answer[0], i);
+                    answer[1] = Math.min(answer[1], j);
+                    answer[2] = Math.max(answer[2], i + 1);
+                    answer[3] = Math.max(answer[3], j + 1);
+                }
+            }
+        }
+        return answer;
+    }
+}
