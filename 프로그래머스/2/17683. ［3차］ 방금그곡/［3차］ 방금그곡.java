2d2e9f8a9e24b5853683415997a00baf09fc0ccd
SITY <126085673+LSBsity@users.noreply.github.com>
2025-06-24 11:58:49 +0900

    [level 2] Title: [3차] 방금그곡, Time: 17.89 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -3,45 +3,46 @@ import java.time.*;
 import java.time.format.*;
 
 class Solution {
+    
     DateTimeFormatter fmt = DateTimeFormatter.ofPattern("HH:mm");
-
-    private String convertMelody(String melody) {
-        return melody
-                .replaceAll("C#", "c")
-                .replaceAll("D#", "d")
-                .replaceAll("F#", "f")
-                .replaceAll("G#", "g")
-                .replaceAll("A#", "a")
-                .replaceAll("B#", "b");
+    
+    private String convert(String before) {
+        return before.replaceAll("C#", "c")
+            .replaceAll("D#", "d")
+            .replaceAll("F#", "f")
+            .replaceAll("G#", "g")
+            .replaceAll("A#", "a")
+            .replaceAll("B#", "b");
     }
-
+    
     public String solution(String m, String[] musicinfos) {
         String answer = "(None)";
-        long maxTime = -1;
-        String target = convertMelody(m); 
-
-        for (String music : musicinfos) {
-            String[] line = music.split(",");
-            LocalTime startTime = LocalTime.parse(line[0], fmt);
-            LocalTime endTime = LocalTime.parse(line[1], fmt);
+        long maxPlaytime = -1;
+        
+        String target = this.convert(m);
+        
+        for (String line : musicinfos) {
+            String[] parsed = line.split(",");
+            
+            LocalTime startTime = LocalTime.parse(parsed[0], fmt);
+            LocalTime endTime = LocalTime.parse(parsed[1], fmt);
             long minutes = Duration.between(startTime, endTime).toMinutes();
-
-            String melody = convertMelody(line[3]);
+            
+            String codes = this.convert(parsed[3]);
+            int length = codes.length();
+            
             StringBuilder sb = new StringBuilder();
-
             for (int i = 0; i < minutes; i++) {
-                sb.append(melody.charAt(i % melody.length()));
+                sb.append(codes.charAt(i % length));
             }
-
-            String played = sb.toString();
-            if (played.contains(target)) {
-                if (minutes > maxTime) {
-                    maxTime = minutes;
-                    answer = line[2];
-                }
+            
+            String codeLine = sb.toString();
+            if (codeLine.contains(target) && maxPlaytime < minutes) {
+                answer = parsed[2];
+                maxPlaytime = minutes;
             }
         }
-
+        
         return answer;
     }
 }
