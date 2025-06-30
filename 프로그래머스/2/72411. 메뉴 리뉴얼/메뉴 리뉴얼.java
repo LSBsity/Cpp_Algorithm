b08d959a0539cd6d852556e615720d373c56caf0
SITY <126085673+LSBsity@users.noreply.github.com>
2025-06-30 15:19:54 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 15.96 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,44 +1,46 @@
 import java.util.*;
-
+ 
 class Solution {
     
-    public void combi(char[] chars, List<Character> temp, int depth, int start, Set<String> result) {
-        if (temp.size() == depth) {
-            List<Character> sorted = new ArrayList<>(temp);
-            Collections.sort(sorted);
+    private void go(char[] chars, List<Character> list, int depth, int start, Set<String> result) {
+        if (list.size() == depth) {
+            List<Character> temp = new ArrayList<>(list);
             StringBuilder sb = new StringBuilder();
-            for (char c : sorted) sb.append(c);
+            for (var c : temp) {
+                sb.append(c);
+            }
             result.add(sb.toString());
             return;
         }
         
         for (int i = start; i < chars.length; i++) {
-            temp.add(chars[i]);
-            this.combi(chars, temp, depth, i + 1, result);
-            temp.remove(temp.size() - 1);
+            list.add(chars[i]);
+            this.go(chars, list, depth, i + 1, result);
+            list.remove(list.size() - 1);
         }
     }
     
     public String[] solution(String[] orders, int[] course) {
-        Map<String, Integer> combiCount = new HashMap<>();
-                
-        for (String order : orders) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (var order : orders) {
             char[] chars = order.toCharArray();
-            Arrays.sort(chars);
-            for (int len : course) {
+            Arrays.sort(chars);           
+            for (var len : course) {
                 Set<String> result = new HashSet<>();
-                this.combi(chars, new ArrayList<>(), len, 0, result);
-                for (String s : result) {
-                    combiCount.put(s, combiCount.getOrDefault(s, 0) + 1);
+                this.go(chars, new ArrayList<>(), len, 0, result);
+                
+                for (var str : result) {
+                    map.put(str, map.getOrDefault(str, 0) + 1);
                 }
             }
         }
-        
-        List<String> answerList = new ArrayList<>();
-        for (int len : course) {
-            int max = 2;
+        List<String> list = new ArrayList<>();
+        for (var len : course) {
             List<String> candidates = new ArrayList<>();
-            for (var entry : combiCount.entrySet()) {
+            int max = 2;
+            
+            for (var entry : map.entrySet()) {
                 if (entry.getKey().length() == len) {
                     if (entry.getValue() > max) {
                         candidates.clear();
@@ -49,10 +51,12 @@ class Solution {
                     }
                 }
             }
-            answerList.addAll(candidates);
-        }
+            
+            list.addAll(candidates);
+        }        
         
-        Collections.sort(answerList);
-        return answerList.toArray(new String[0]);
+        Collections.sort(list);
+
+        return list.toArray(new String[0]);
     }
 }
