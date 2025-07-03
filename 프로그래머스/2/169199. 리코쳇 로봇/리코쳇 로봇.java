380c75e5cb872d002aff91291828bb3578423293
SITY <126085673+LSBsity@users.noreply.github.com>
2025-07-03 17:01:42 +0900

    [level 2] Title: 리코쳇 로봇, Time: 3.45 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,24 +1,24 @@
 import java.util.*;
 
 class Solution {
-    
+
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
     
     char[][] map;
     int[][] visited;
-
     int[][] pos = new int[2][2];
     
     public int solution(String[] board) {
         int answer = -1;
+        
         map = new char[board.length][board[0].length()];
         visited = new int[board.length][board[0].length()];
- 
+
         for (int i = 0; i < board.length; i++) {
-            char[] line = board[i].toCharArray();
-            for (int j = 0; j < line.length; j++) {
-                map[i][j] = line[j];
+            char[] chars = board[i].toCharArray();
+            for (int j = 0; j < chars.length; j++) {
+                map[i][j] = chars[j];
                 if (map[i][j] == 'R') {
                     pos[0][0] = i;
                     pos[0][1] = j;
@@ -28,17 +28,15 @@ class Solution {
                 }
             }
         }
-    
+        
         Queue<Position> q = new LinkedList<>();
         q.offer(new Position(pos[0][0], pos[0][1], 0));
-        visited[pos[0][0]][pos[0][1]] = 0;
         
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             Position current = q.poll();
             
             if (current.x == pos[1][0] && current.y == pos[1][1]) {
                 answer = current.count;
-                System.out.println(answer);
                 break;
             }
             
@@ -48,21 +46,19 @@ class Solution {
                 while (true) {
                     int tx = nx + dx[i];
                     int ty = ny + dy[i];
-                    
-                    if (tx < 0 || ty < 0 || tx >= map.length || ty >= map[0].length) break;
+                    if (tx < 0 || ty < 0 || tx >= board.length || ty >= board[0].length()) break;
                     if (map[tx][ty] == 'D') break;
-                    
                     nx = tx;
                     ny = ty;
-                }
-                
+                } 
+
                 if (visited[nx][ny] == 0 || visited[nx][ny] > current.count + 1) {
                     visited[nx][ny] = current.count + 1;
                     q.offer(new Position(nx, ny, current.count + 1));
                 }
             }
         }
-    
+        
         return answer;
     }
     
@@ -70,11 +66,11 @@ class Solution {
         int x;
         int y;
         int count;
-        
         public Position(int x, int y, int count) {
             this.x = x;
             this.y = y;
             this.count = count;
         }
     }
+
 }
