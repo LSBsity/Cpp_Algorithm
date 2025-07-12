f53d6444d4f94d37577d1a6d3b2a00dd7329a6b2
SITY <126085673+LSBsity@users.noreply.github.com>
2025-07-12 13:37:06 +0900

    [level 3] Title: 징검다리 건너기, Time: 49.48 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,33 +1,39 @@
 class Solution {
     public int solution(int[] stones, int k) {
-        int left = 1;
-        int right = 200000000; 
-
         int answer = 0;
+        
+        int left = 1;
+        int right = 200_000_000;
+        
         while (left <= right) {
-            int mid = (left + right) / 2;
-            System.out.println(left + " " + mid + " " + right + " -> " + answer);
-
+            int mid = (right + left) / 2;
+            
+            System.out.println(left + " " + mid + " " + right);
             if (this.canCross(stones, k, mid)) {
-                answer = mid;    
-                left = mid + 1;   
+                answer = mid;
+                left = mid + 1;
             } else {
-                right = mid - 1;  
+                right = mid - 1;
             }
         }
+        
         return answer;
     }
-
+    
     private boolean canCross(int[] stones, int k, int mid) {
+        
         int count = 0;
-        for (int stone : stones) {
+        for (var stone : stones) {
             if (stone - mid < 0) {
-                count++; 
-                if (count >= k) return false; 
+                count++;
+                if (count >= k) {
+                    return false;
+                }
             } else {
                 count = 0;
             }
         }
+        
         return true;
     }
 }
