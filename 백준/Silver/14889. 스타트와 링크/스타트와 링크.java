bd18bbf7bb1be95ad8517689308e1eaceebf0338
SITY <126085673+LSBsity@users.noreply.github.com>
2025-07-25 13:06:54 +0900

    [Silver I] Title: 스타트와 링크, Time: 280 ms, Memory: 19200 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -2,54 +2,60 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    static int min = Integer.MAX_VALUE;
+
     static int N;
     static int[][] arr;
     static boolean[] visited;
-    static int min = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws Exception {
+    static StringTokenizer st = null;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
+
         arr = new int[N][N];
         visited = new boolean[N];
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        visited[0] = true;  
-        dfs(1, 1);
+        visited[0] = true;
+        go(1, 1);
+
         System.out.println(min);
     }
 
-    private static void dfs(int idx, int depth) {
+    private static void go(int idx, int depth) {
         if (depth == N / 2) {
-            calcDiff();
+            calc();
             return;
         }
 
         for (int i = idx; i < N; i++) {
             visited[i] = true;
-            dfs(i + 1, depth + 1);
+            go(i + 1, depth + 1);
             visited[i] = false;
         }
     }
 
-    private static void calcDiff() {
-        int teamA = 0, teamB = 0;
+    private static void calc() {
+        int sumA = 0, sumB = 0;
         for (int i = 0; i < N - 1; i++) {
             for (int j = i + 1; j < N; j++) {
                 if (visited[i] && visited[j]) {
-                    teamA += arr[i][j] + arr[j][i];
+                    sumA += arr[i][j] + arr[j][i];
                 } else if (!visited[i] && !visited[j]) {
-                    teamB += arr[i][j] + arr[j][i];
+                    sumB += arr[i][j] + arr[j][i];
                 }
             }
         }
-        min = Math.min(min, Math.abs(teamA - teamB));
+        min = Math.min(min, Math.abs(sumA - sumB));
     }
+
 }
