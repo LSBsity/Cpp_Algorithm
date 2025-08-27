acfd1ad4ef4972e5ec73b1f2c5b88c5e1b65e6b3
SITY <126085673+LSBsity@users.noreply.github.com>
2025-08-27 09:49:40 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 956 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -2,20 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st = null;
 
     static StringBuilder sb = new StringBuilder();
 
-    static int T; // 테스트 케이스 수
-    static int N; // 고객 수
-    static int minDist; // 최소 거리 저장
+    static int T, N;
 
-    static int[] company = new int[2]; // 회사 좌표
-    static int[] home = new int[2]; // 집 좌표
-    static int[][] clients; // 고객 좌표
+    static int minDist;
 
+    static int[] home = { 0, 0 };
+    static int[] company = { 0, 0 };
+
+    static int[][] client;
     static boolean[] visited;
 
     public static void main(String[] args) throws Exception {
@@ -23,57 +22,49 @@ public class Solution {
 
         for (int t = 1; t <= T; t++) {
             N = Integer.parseInt(br.readLine());
-            clients = new int[N][2];
-            visited = new boolean[N];
+
+            client = new int[N][2];
+            visited = new boolean[N + 2];
             minDist = Integer.MAX_VALUE;
 
             st = new StringTokenizer(br.readLine());
 
-            // 회사 좌표
             company[0] = Integer.parseInt(st.nextToken());
             company[1] = Integer.parseInt(st.nextToken());
 
-            // 집 좌표
             home[0] = Integer.parseInt(st.nextToken());
             home[1] = Integer.parseInt(st.nextToken());
 
-            // 고객 좌표 입력
             for (int i = 0; i < N; i++) {
-                clients[i][0] = Integer.parseInt(st.nextToken());
-                clients[i][1] = Integer.parseInt(st.nextToken());
+                client[i][0] = Integer.parseInt(st.nextToken());
+                client[i][1] = Integer.parseInt(st.nextToken());
             }
 
-            // 완전탐색 시작
-            dfs(0, company[0], company[1], 0);
+            go(company[0], company[1], 0, 0);
 
-            sb.append("#").append(t).append(" ").append(minDist).append("\n");
+            sb.append('#')
+                    .append(t)
+                    .append(' ')
+                    .append(minDist)
+                    .append('\n');
         }
 
-        System.out.print(sb);
+        System.out.println(sb);
     }
 
-    // depth : 방문한 고객 수
-    // x, y : 현재 위치
-    // dist : 지금까지의 거리
-    static void dfs(int depth, int x, int y, int dist) {
-        // 현재까지 거리가 이미 최소 거리보다 크면 더 볼 필요 없음 (가지치기)
-        // if (dist >= minDist)
-        //     return;
-
-        // 모든 고객을 다 방문했으면 집까지 이동 후 최소값 갱신
-        if (depth == N) {
+    public static void go(int x, int y, int depth, int dist) {
+        if (depth >= N) {
             dist += Math.abs(x - home[0]) + Math.abs(y - home[1]);
             minDist = Math.min(minDist, dist);
             return;
         }
 
-        // 방문하지 않은 고객 탐색
         for (int i = 0; i < N; i++) {
             if (!visited[i]) {
+                int nx = client[i][0];
+                int ny = client[i][1];
                 visited[i] = true;
-                int nx = clients[i][0];
-                int ny = clients[i][1];
-                dfs(depth + 1, nx, ny, dist + Math.abs(x - nx) + Math.abs(y - ny));
+                go(nx, ny, depth + 1, dist + Math.abs(x - nx) + Math.abs(y - ny));
                 visited[i] = false;
             }
         }
