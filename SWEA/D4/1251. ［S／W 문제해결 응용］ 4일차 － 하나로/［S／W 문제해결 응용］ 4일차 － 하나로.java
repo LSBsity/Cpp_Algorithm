cba65409753557a88c5637feae55fc257ffb3003
SITY <126085673+LSBsity@users.noreply.github.com>
2025-08-28 11:33:13 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 756 ms, Memory: 78,368 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.util.*;
 
-// using prim algorithm
 public class Solution {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,20 +9,43 @@ public class Solution {
     static StringBuilder sb = new StringBuilder();
 
     static int T, N;
-
-    static int[] dx;
-    static int[] dy;
-
     static double tax;
 
-    static List<Node>[] adjList;
+    static int[][] island;
+    static Edge[] edges;
+
     static boolean[] visited;
+    static int[] parents;
 
     public static void main(String[] args) throws Exception {
         T = Integer.parseInt(br.readLine());
 
         for (int t = 1; t <= T; t++) {
-            init();
+            N = Integer.parseInt(br.readLine());
+
+            island = new int[2][N];
+            visited = new boolean[N];
+
+            parents = new int[N];
+            for (int i = 0; i < N; i++) {
+                parents[i] = i;
+            }
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                island[0][i] = Integer.parseInt(st.nextToken());
+            }
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                island[1][i] = Integer.parseInt(st.nextToken());
+            }
+
+            tax = Double.parseDouble(br.readLine());
+
+            edges = buildEdges();
+            Arrays.sort(edges);
+
             long result = go();
 
             sb.append('#')
@@ -36,88 +58,71 @@ public class Solution {
         System.out.println(sb);
     }
 
+    public static Edge[] buildEdges() {
+        Edge[] newNodes = new Edge[(N * (N - 1)) / 2];
+
+        for (int i = 0, idx = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                long nx = island[0][i] - island[0][j];
+                long ny = island[1][i] - island[1][j];
+
+                long w = nx * nx + ny * ny;
+                newNodes[idx++] = new Edge(i, j, w);
+            }
+        }
+
+        return newNodes;
+    }
+
+    public static int find(int a) {
+        if (parents[a] == a)
+            return a;
+
+        return parents[a] = find(parents[a]);
+    }
+
+    public static boolean union(int a, int b) {
+        int aRoot = find(a);
+        int bRoot = find(b);
+
+        if (aRoot == bRoot)
+            return false;
+
+        parents[bRoot] = aRoot;
+        return true;
+    }
+
     public static long go() {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-
-        pq.offer(new Node(1, 0));
-
+        // todo kruskal
         long sum = 0;
         int count = 0;
 
-        while (!pq.isEmpty()) {
-            Node current = pq.poll();
-
-            if (visited[current.vertex])
+        for (Edge edge : edges) {
+            if (!union(edge.from, edge.to))
                 continue;
-
-            visited[current.vertex] = true;
-            sum += current.weight;
+            sum += edge.weight;
             count++;
 
-            if (count == N)
+            if (count == N - 1)
                 break;
-
-            for (Node next : adjList[current.vertex]) {
-                if (!visited[next.vertex]) {
-                    pq.offer(next);
-                }
-            }
         }
 
         return sum;
     }
 
-    private static void init() throws IOException {
-        N = Integer.parseInt(br.readLine());
-
-        dx = new int[N];
-        dy = new int[N];
-        adjList = new List[N + 1];
-        visited = new boolean[N + 1];
-
-        for (int i = 0; i < N + 1; i++) {
-            adjList[i] = new ArrayList<>();
-        }
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            dx[i] = Integer.parseInt(st.nextToken());
-        }
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            dy[i] = Integer.parseInt(st.nextToken());
-        }
-
-        tax = Double.parseDouble(br.readLine());
-
-        buildEdges();
-    }
-
-    public static void buildEdges() {
-
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                long nx = dx[i] - dx[j];
-                long ny = dy[i] - dy[j];
-
-                long w = nx * nx + ny * ny;
-                adjList[i].add(new Node(j, w));
-                adjList[j].add(new Node(i, w));
-            }
-        }
-    }
-
-    static class Node implements Comparable<Node> {
-        int vertex;
+    static class Edge implements Comparable<Edge> {
+        int from;
+        int to;
         long weight;
 
-        public Node(int vertex, long weight) {
-            this.vertex = vertex;
+        public Edge(int from, int to, long weight) {
+            this.from = from;
+            this.to = to;
             this.weight = weight;
         }
 
         @Override
-        public int compareTo(Node o) {
+        public int compareTo(Edge o) {
             return Long.compare(this.weight, o.weight);
         }
     }
