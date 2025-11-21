326add087b2acc8a2e1bb56dce2459390d8892c9
SITY <126085673+LSBsity@users.noreply.github.com>
2025-11-21 17:34:57 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 73 ms, Memory: 24,960 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -1,42 +1,44 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st = null;
     static StringBuilder sb = new StringBuilder();
 
-    static int T;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int T, N;
+    static int max = 2046; // 1111111110
 
     public static void main(String[] args) throws Exception {
         T = Integer.parseInt(br.readLine());
 
-        int bit = (1 << 10) - 1;
-
         for (int t = 1; t <= T; t++) {
-            int N = Integer.parseInt(br.readLine());
+            N = Integer.parseInt(br.readLine());
+            int sheep = N;
 
-            int count = 0;
-            int check = 0;
+            int bit = 0;
+            int cnt = 0;
+            while (bit != max) {
+                int temp = sheep;
+                while (temp != 0) {
+                    int rem = temp % 10;
+                    temp /= 10;
 
-            for (count = 1;; count++) {
-                char[] chars = String.valueOf(N * count).toCharArray();
-
-                for (char c : chars) {
-                    int num = c - '0';
-
-                    check |= (1 << num);
+                    bit |= 1 << rem + 1;
                 }
-
-                if (bit == check)
-                    break;
+                sheep += N;
+                cnt++;
             }
 
-            sb.append(String.format("#%d %d", t, N * count))
+            sb.append('#')
+                    .append(t)
+                    .append(' ')
+                    .append(cnt * N)
                     .append('\n');
         }
-        
+
         System.out.println(sb);
     }
 
