ac8f3427f39c0e350f1477289ced1dba3ee02dc7
SITY <126085673+LSBsity@users.noreply.github.com>
2026-02-19 22:05:52 +0900

    [Gold IV] Title: 여행 가자, Time: 136 ms, Memory: 18360 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -10,7 +10,8 @@ public class Main {
     static StringTokenizer st;
 
     static int T, N, M;
-    static int[] parent, cities;
+
+    static int[] parent;
 
     public static void main(String[] args) throws Exception {
         T = 1;
@@ -18,9 +19,8 @@ public class Main {
         for (int t = 1; t <= T; t++) {
             N = Integer.parseInt(br.readLine());
             M = Integer.parseInt(br.readLine());
-
             parent = new int[N + 1];
-            cities = new int[M];
+
             for (int i = 1; i <= N; i++) {
                 parent[i] = i;
             }
@@ -29,45 +29,33 @@ public class Main {
                 st = new StringTokenizer(br.readLine());
 
                 for (int j = 1; j <= N; j++) {
-                    int v = Integer.parseInt(st.nextToken());
-                    if (v == 1) {
-                        union(i, j);
-                    }
+                    if (st.nextToken().equals("1")) union(i, j);
                 }
             }
 
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < M; i++) {
-                cities[i] = Integer.parseInt(st.nextToken());
-            }
+            int first = find(Integer.parseInt(st.nextToken()));
 
-            boolean answer = true;
-            for (int i = 0; i < M - 1; i++) {
-                int aa = find(cities[i]);
-                int bb = find(cities[i + 1]);
-                if (aa != bb) {
-                    answer = false;
-                    break;
+            while (st.hasMoreTokens()) {
+                if (find(Integer.parseInt(st.nextToken())) != first) {
+                    System.out.println("NO");
+                    return;
                 }
             }
-            sb.append(answer ? "YES" : "NO");
         }
+        System.out.print("YES");
+    }
 
-        System.out.print(sb);
+
+    private static int find(int x) {
+        if (parent[x] == x) return x;
+
+        return parent[x] = find(parent[x]);
     }
 
     private static void union(int a, int b) {
-        int aa = find(a);
-        int bb = find(b);
-
-        parent[bb] = aa;
-    }
-
-    private static int find(int x) {
-        if (parent[x] == x) {
-            return x;
-        }
-
-        return parent[x] = find(parent[x]);
+        a = find(a);
+        b = find(b);
+        if (a != b) parent[b] = a;
     }
 }
