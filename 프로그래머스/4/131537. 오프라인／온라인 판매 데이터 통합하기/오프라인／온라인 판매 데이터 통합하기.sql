8243ba1d2546d5d10fc9ee96a93e0d5dd7dfb76d
SITY <126085673+LSBsity@users.noreply.github.com>
2026-04-24 20:34:50 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,24 +1,21 @@
-select date_format(sales_date, '%Y-%m-%d') as sales_date,
-       product_id,
-       user_id,
-       sales_amount
-from (
-    select date_format(ons.sales_date, '%Y-%m-%d') as sales_date,
-       ons.product_id,
-       ons.user_id,
-       ons.sales_amount
-    from online_sale ons
-    where year(ons.sales_date) = 2022 and
-          month(ons.sales_date) = 3
-    
-    union all
-    
-    select date_format(offs.sales_date, '%Y-%m-%d') as sales_date,
-       offs.product_id,
-       null as user_id,
-       offs.sales_amount
-    from offline_sale offs
-    where year(offs.sales_date) = 2022 and
-          month(offs.sales_date) = 3
-) as total_sale
-order by sales_date asc, product_id asc, user_id asc;
+select
+  ons.sales_date,
+  ons.product_id,
+  ons.user_id,
+  ons.sales_amount
+from online_sale ons
+where ons.sales_date >= '2022-03-01' and
+  ons.sales_date < '2022-04-01'
+  
+union 
+
+select
+  offs.sales_date,
+  offs.product_id,
+  NULL,
+  offs.sales_amount
+from offline_sale offs
+where offs.sales_date >= '2022-03-01' and
+  offs.sales_date < '2022-04-01'
+  
+order by sales_date, product_id, user_id;
