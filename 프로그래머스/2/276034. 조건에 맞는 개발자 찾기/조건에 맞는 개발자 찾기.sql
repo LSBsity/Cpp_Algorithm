9a6eb2309354595667cd1827609980ecfe5ee7c1
SITY <126085673+LSBsity@users.noreply.github.com>
2026-05-05 18:49:36 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,11 +1,13 @@
-select d.id,
-       d.email,
-       d.first_name,
-       d.last_name
+select
+  d.id,
+  d.email,
+  d.first_name,
+  d.last_name
 from developers d
 where d.skill_code & (
-    select bit_or(c.code)
+    select sum(c.code)
     from skillcodes c
     where c.name in ('Python', 'C#')
 )
-order by d.id asc;
+order by d.id;
+
