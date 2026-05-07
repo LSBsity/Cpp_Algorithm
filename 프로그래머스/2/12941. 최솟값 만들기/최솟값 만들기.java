766fa414c29a0f6fb4be70b3c5028b98993f01fd
SITY <126085673+LSBsity@users.noreply.github.com>
2026-05-07 21:11:07 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.82 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,19 +1,14 @@
-import java.util.Arrays;
+import java.util.*;
 
-class Solution
-{
-    public int solution(int[] arr1, int[] arr2)
-    {
-        int answer = 0;
+class Solution{
+    public int solution(int[] A, int[] B){
+        Arrays.sort(A); Arrays.sort(B);
         
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        
-        for (int i = 0; i < arr1.length; i++) {
-            answer += arr1[i] * arr2[arr2.length - i - 1];
+        int sum = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i] * B[B.length - i - 1];
         }
         
-        
-        return answer;
+        return sum;
     }
 }
