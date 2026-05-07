90a4a767c6219a3ae8602583974635ccd340f10b
SITY <126085673+LSBsity@users.noreply.github.com>
2026-05-08 00:01:04 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,26 @@
-import java.util.Arrays;
-
 class Solution {
-
     public int[] solution(int brown, int yellow) {
-        for (int i = yellow; i >= 1; i--) {
-            for (int j = 2; j <= yellow; j++) {
-                if (i * j == yellow) {
-                    if ((i + 2) * (j + 2) == brown + yellow) {
-                        return new int[]{Math.max(i, j) + 2, Math.min(i, j) + 2};
-                    }
-                } else if (i * j >= yellow) {
-                    break;
+        int blocks = brown + yellow;
+        
+        int first = 3;
+
+        while (first < blocks) {
+            int b = first;
+
+            if (blocks % b == 0) {
+                int a = blocks / b;
+                
+                int upDown = a * 2;
+                int side = (b - 2) * 2;
+                
+                if (upDown + side + yellow == blocks) {
+                    return new int[]{a, b};
                 }
             }
+            
+            first++;
         }
-        return new int[]{yellow + 2, yellow + 2};
+    
+        return null;
     }
 }
