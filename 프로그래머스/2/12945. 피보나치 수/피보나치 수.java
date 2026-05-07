98580582f68231094b2e932720b493f83837a925
SITY <126085673+LSBsity@users.noreply.github.com>
2026-05-07 22:36:06 +0900

    [level 2] Title: 피보나치 수, Time: 2.03 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,18 +1,13 @@
 class Solution {
-
-    private long fino(int n) {
-        long[] dp = new long[100001];
-        dp[0] = 0L;
-        dp[1] = 1L;
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
-        }
-
-        return dp[n];
-    }
-
     public int solution(int n) {
-        return (int) this.fino(n);
+        int[] arr = new int[100_001];
+        
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = (arr[i - 1] % 1234567) + (arr[i - 2] % 1234567);
+        }
+        
+        return arr[n] % 1234567;
     }
 }
