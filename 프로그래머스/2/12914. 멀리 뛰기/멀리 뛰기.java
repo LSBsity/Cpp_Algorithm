8bc14ec7c4ab7fe490e6fbdbc72edbc6b31019bf
SITY <126085673+LSBsity@users.noreply.github.com>
2026-05-08 14:39:15 +0900

    [level 2] Title: 멀리 뛰기, Time: 1.60 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,13 +1,15 @@
 class Solution {
     public long solution(int n) {
-        long[] arr = new long[2001];
-
+        long answer = 0;
+        
+        int[] arr = new int[n + 2];
+        
         arr[0] = 0;
         arr[1] = 1;
         for (int i = 2; i <= n + 1; i++) {
             arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
         }
-
-        return arr[n + 1];
+        
+        return arr[n + 1] % 1234567;
     }
 }
