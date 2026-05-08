94189fc90b3fbcd775ca143514ad1d586bd875a1
SITY <126085673+LSBsity@users.noreply.github.com>
2026-05-08 12:11:47 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -1,13 +1,7 @@
-class Solution {
+import java.util.*;
 
-    public int solution(int n) {
-        String binaryString = Integer.toBinaryString(n);
-        int count = 0;
-        for (int i = 0; i < binaryString.length(); i++) {
-            if (binaryString.charAt(i) == '1') {
-                count++;
-            }
-        }
-        return count;
+public class Solution {
+    public int solution(int val) {
+        return Integer.bitCount(val);
     }
 }
