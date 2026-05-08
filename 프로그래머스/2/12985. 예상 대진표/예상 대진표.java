0441083ad8829d38f79863cbae2aaf2b2b974128
SITY <126085673+LSBsity@users.noreply.github.com>
2026-05-08 20:29:14 +0900

    [level 2] Title: 예상 대진표, Time: 18.75 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,34 +1,31 @@
-class Solution {
+import java.util.*;
 
-    public int solution(int n, int a, int b) {
+class Solution{
+    public int solution(int n, int a, int b){
+        int answer = 0;
+
         int[] arr = new int[n];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i + 1;
+        for (int i = 0; i < n; i++) {
+            if (i == a - 1 || i == b - 1) arr[i] = -1;
+            else arr[i] = i + 1;
         }
-        arr[a - 1] = Integer.MAX_VALUE;
-        arr[b - 1] = Integer.MAX_VALUE;
-
-        int round = 0;
-        while (true) {
-            round++;
-            if (check(arr)) {
-                break;
+        
+        int N = Integer.numberOfTrailingZeros(n);
+    
+        for (int i = 1; i <= N; i++) {
+            int[] temp = new int[n / 2];
+            for (int j = 0, idx = 0; j < arr.length; j += 2) {
+                if (arr[j] == arr[j + 1]) {
+                    return i;
+                }
+                temp[idx++] = Math.min(arr[j], arr[j + 1]);
             }
-            int[] temp = new int[n /= 2];
-            for (int i = 0, idx = 0; i < temp.length; i++, idx += 2) {
-                temp[i] = Math.max(arr[idx], arr[idx + 1]);
-            }
-            arr = temp;
+            arr = new int[n / 2];
+            System.arraycopy(temp, 0, arr, 0, arr.length);
+            n /= 2;
         }
-        return round;
-    }
-
-    private boolean check(int[] arr) {
-        for (int i = 0; i < arr.length; i += 2) {
-            if (arr[i] == arr[i + 1]) {
-                return true;
-            }
-        }
-        return false;
+        
+        return -1;
     }
 }
+    
