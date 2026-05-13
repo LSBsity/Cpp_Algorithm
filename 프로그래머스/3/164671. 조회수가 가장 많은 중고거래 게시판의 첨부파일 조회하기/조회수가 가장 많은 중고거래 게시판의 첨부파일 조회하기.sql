dbba97359e28f5d0c12360895cc4f343f841df57
SITY <126085673+LSBsity@users.noreply.github.com>
2026-05-13 17:17:36 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,8 +1,10 @@
-select concat('/home/grep/src/', b.board_id, '/', f.file_id, f.file_name, f.file_ext) as file_path
+select
+    concat('/home/grep/src/', b.board_id, '/', f.file_id, f.file_name, f.file_ext)
+    as file_path
 from used_goods_board b
-    join used_goods_file f on b.board_id = f.board_id
+  join used_goods_file f on b.board_id = f.board_id
 where b.views = (
-    select max(sub_b.views)
-    from used_goods_board sub_b
+    select max(b2.views)
+    from used_goods_board b2
 )
 order by f.file_id desc;
