52d9460125cd1b78900ff74c136ad52adafdabdb
SITY <126085673+LSBsity@users.noreply.github.com>
2026-06-04 16:48:14 +0900

    [level 3] Title: 없어진 기록 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,7 +1,7 @@
-select ao.animal_id,
-       ao.name
+select
+  ao.animal_id,
+  ao.name
 from animal_outs ao
-    left join animal_ins ai on ao.animal_id = ai.animal_id
-where ao.animal_id is not null and
-      ai.animal_id is null
-order by ai.animal_id asc;
+  left join animal_ins ai on ao.animal_id = ai.animal_id
+where ai.animal_id is null
+order by ao.animal_id asc;
