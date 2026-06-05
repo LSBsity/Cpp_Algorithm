efde931cd3bee2e69294777caf20b5cd09b09158
SITY <126085673+LSBsity@users.noreply.github.com>
2026-06-05 16:46:00 +0900

    [level 1] Title: 최소직사각형, Time: 0.85 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,22 +1,33 @@
-import java.util.Arrays;
-
 class Solution {
-    public int solution(int[][] sizes) {
+    public int solution(int[][] cards) {
         int answer = 0;
         
-        
-        int xMax = -1;
-        int yMax = -1;
-        for (int[] arr : sizes) {
-            Arrays.sort(arr);
-            if (arr[0] > xMax) {
-                xMax = arr[0];
-            }
-            if (arr[1] > yMax) {
-                yMax = arr[1];
+        int rowMax = 0x80000000;
+        int colMax = 0x80000000;
+        for (int[] card : cards) {
+            int row = card[0];
+            int col = card[1];
+            
+            if (row < col) {
+                card[0] = col;
+                card[1] = row;
             }
         }
+
+        for (int[] card : cards) {
+            int row = card[0];
+            int col = card[1];
+            
+            rowMax = Math.max(rowMax, row);
+            colMax = Math.max(colMax, col);
+        }
         
-        return xMax * yMax;
+        return rowMax * colMax;
     }
 }
+
+
+/**
+1. 가장 큰 가로 세로 길이 값들을 찾는다?
+2. 하나씩 체크하면서 가로 세로 길이를 바꿨을 때 반대 길이보다 작으면서 가장 작은 걸 찾는다
+**/
