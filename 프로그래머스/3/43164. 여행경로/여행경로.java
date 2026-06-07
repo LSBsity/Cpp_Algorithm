8ba87bcf2a56c00a19c91d16a79ab714365e0604
SITY <126085673+LSBsity@users.noreply.github.com>
2026-06-07 15:11:43 +0900

    [level 3] Title: 여행경로, Time: 0.23 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,38 +1,50 @@
 import java.util.*;
 
 class Solution {
+    
     static Map<String, List<String>> graph = new HashMap<>();
     static List<String> path = new ArrayList<>();
-    static int total;
-
+    static int size;
+    
     public String[] solution(String[][] tickets) {
-        total = tickets.length;
-        for (String[] t : tickets) {
-            graph.putIfAbsent(t[0], new ArrayList<>());
-            graph.get(t[0]).add(t[1]);
+        size = tickets.length;
+        
+        for (String[] ticket : tickets) {
+            String from = ticket[0];
+            String to = ticket[1];
+            
+            graph.putIfAbsent(from, new ArrayList<>());
+            graph.get(from).add(to);
         }
-        for (List<String> v : graph.values()) Collections.sort(v);
-
+        
+        for (List<String> ticket : graph.values()) {
+            Collections.sort(ticket);
+        }
+        
         path.add("ICN");
         go("ICN");
+        
         return path.toArray(new String[0]);
     }
-
-    static boolean go(String cur) {
-        if (path.size() == total + 1) return true;
-
+    
+    private static boolean go(String cur) {
+        if (path.size() == size + 1) {
+            return true;
+        }
+        
         List<String> nexts = graph.get(cur);
         if (nexts == null) return false;
-
+        
         for (int i = 0; i < nexts.size(); i++) {
-            String next = nexts.remove(i);   // 티켓 한 장 사용
+            String next = nexts.remove(i);
             path.add(next);
-
+            
             if (go(next)) return true;
-
-            path.remove(path.size() - 1);    // 백트래킹
-            nexts.add(i, next);              // 티켓 도로 넣기
+            
+            nexts.add(i, next);
+            path.remove(path.size() - 1);
         }
+        
         return false;
     }
 }
