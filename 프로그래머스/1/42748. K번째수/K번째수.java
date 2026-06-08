b4a6792763ae7cb9db7844d2897292d1039a876f
SITY <126085673+LSBsity@users.noreply.github.com>
2026-06-08 17:30:15 +0900

    [level 1] Title: K번째수, Time: 1.58 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,21 +1,26 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         int idx = 0;
-
-        for (int[] arr : commands) {
-            int start = arr[0];
-            int end = arr[1];
-            int target = arr[2];
-
-            int[] temp = Arrays.copyOfRange(array, start - 1, end);
+        
+        for (int[] command : commands) {
+            int start = command[0];
+            int end = command[1];
+            int len = command[2];
+            
+            int[] temp = new int[end - start + 1];
+            
+            System.arraycopy(array, start - 1, temp, 0, end - start + 1);
+            
             Arrays.sort(temp);
-            answer[idx++] = temp[target - 1];
+            System.out.println(Arrays.toString(temp));
+            
+            answer[idx++] = temp[len - 1];
         }
-
+        
+        
         return answer;
     }
 }
