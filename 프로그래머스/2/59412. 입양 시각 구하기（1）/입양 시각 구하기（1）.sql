aa47d66b30d90a66d83a764dbfc5e5f00853ef63
SITY <126085673+LSBsity@users.noreply.github.com>
2026-06-09 16:40:52 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,6 +1,7 @@
-select hour(ao.datetime) as hour,
-       count(ao.datetime) as count
+select
+  hour(ao.datetime) as hour,
+  count(ao.animal_id) as count
 from animal_outs ao
 where hour(ao.datetime) between 9 and 19
 group by hour(ao.datetime)
-order by hour(ao.datetime) asc;
+order by hour asc;
