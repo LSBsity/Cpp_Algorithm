496f4c4f4781984e28c807d0c6f5b49945f9ed11
SITY <126085673+LSBsity@users.noreply.github.com>
2026-06-13 16:58:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 54.83 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,20 +2,21 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        String answer = "";
         Map<String, Integer> map = new HashMap<>();
-        for (var str : participant) {
-            map.put(str, map.getOrDefault(str, 0) + 1);
-        } 
         
-        for (var str : completion) {
+        for (String str : participant) {
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+        
+        for (String str : completion) {
             map.put(str, map.get(str) - 1);
         }
-        for (var str : map.keySet()) {
-            if (map.get(str) != 0) {
-                return str;
-            }
+        
+        for (String key : map.keySet()) {
+            if (map.get(key) == 1) return key;
         }
         
-        return "";
+        return answer;
     }
 }
