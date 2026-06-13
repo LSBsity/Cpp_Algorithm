d1f08e95c7579a63871962f2325a7016562c3113
SITY <126085673+LSBsity@users.noreply.github.com>
2026-06-13 18:46:00 +0900

    [level 1] Title: 폰켓몬, Time: 3.87 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,11 +1,36 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> set = Arrays.stream(nums).boxed()
-            .collect(Collectors.toSet());
+        Map<Integer, Integer> map = new HashMap<>();
         
-        return Math.min(nums.length / 2, set.size());
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        int n = nums.length / 2;
+        int cnt = 0;
+        
+        if (map.size() >= n) {
+            return n;
+        }
+        
+        Set<Integer> set = new HashSet<>();
+        while (cnt < n) {
+            for (int key : map.keySet()) {
+                if (map.get(key) == 0) {
+                    continue;
+                }
+                
+                map.put(key, map.get(key) - 1);
+                set.add(key);
+                cnt++;
+                
+                if (cnt == n) break;
+            }    
+        }
+        
+        
+        return set.size();
     }
 }
