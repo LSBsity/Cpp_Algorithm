621305ae441c19ea57add05f4ed92eadb5c82a31
SITY <126085673+LSBsity@users.noreply.github.com>
2026-06-15 16:35:51 +0900

    [level 2] Title: 택배상자, Time: 140.33 ms, Memory: 159 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,28 +1,41 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] order) {
-        Deque<Integer> mains = new ArrayDeque<>();
-        for (int i = order.length; i >= 1; i--) {
-            mains.push(i);
-        }
-        Deque<Integer> subs = new ArrayDeque<>();
+
+    public int solution(int[] arr) {
+        int answer = 0;
+        Queue<Integer> main = new LinkedList<>();
+        Deque<Integer> sub = new ArrayDeque<>();
         
-        int idx = 0, answer = 0;
-        while (!mains.isEmpty()) {
-            int value = mains.pop();
-            if (value != order[idx]) { 
-                if (subs.isEmpty() || subs.peek() != order[idx]) {
-                    subs.push(value);              
+        for (int i = 1; i <= arr.length; i++) {
+            main.offer(i);
+        }
+
+        int i = 0;
+        while (i < arr.length) {
+            int should = arr[i];
+            
+            
+            if (!main.isEmpty() && main.peek() != should) {
+                if (!sub.isEmpty() && sub.peek() != should) {
+                    sub.push(main.poll());
+                } else if (!sub.isEmpty() && sub.peek() == should) {
+                    sub.pop();
+                    answer++;
+                    i++;
+                } else {
+                    sub.push(main.poll());
                 }
-            } else if (value == order[idx]) {
-                answer++;          
-                idx++;
-            }    
-            while (!subs.isEmpty() && subs.peek() == order[idx]) {
-                subs.pop();
+            } else if (!main.isEmpty() && main.peek() == should) {
+                main.poll();
                 answer++;
-                idx++;    
+                i++;
+            } else if (!sub.isEmpty() && sub.peek() == should) {
+                sub.pop();
+                answer++;
+                i++;
+            } else {
+                break;
             }
         }
         
