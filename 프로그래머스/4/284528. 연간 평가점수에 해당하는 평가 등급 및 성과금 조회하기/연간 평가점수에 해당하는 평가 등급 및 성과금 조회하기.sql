e5c8eae0147843f915ecd176f35b8e807480b106
SITY <126085673+LSBsity@users.noreply.github.com>
2026-06-15 15:55:54 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,17 +1,26 @@
-select e.emp_no,
-       e.emp_name,
-       (case
-            when avg(g.score) >= 96 then 'S'
-            when avg(g.score) >= 90 then 'A'
-            when avg(g.score) >= 80 then 'B'
-            else 'C'
-       end) as grade,
-       (case
-            when avg(g.score) >= 96 then e.sal * 0.2
-            when avg(g.score) >= 90 then e.sal * 0.15
-            when avg(g.score) >= 80 then e.sal * 0.1
-            else 0
-       end) as bonus
+select
+  e.emp_no,
+  e.emp_name,
+  case
+    when score >= 96 then 'S'
+    when score >= 90 then 'A'
+    when score >= 80 then 'B'
+    else 'C'
+  end as 'grade',
+  case
+    when score >= 96 then e.sal * 0.2
+    when score >= 90 then e.sal * 0.15
+    when score >= 80 then e.sal * 0.1
+    else 0
+  end as 'bonus'
 from hr_employees e
-    join hr_grade g on e.emp_no = g.emp_no
-group by e.emp_no;
+join (
+  select
+    g.emp_no,
+    avg(g.score) as score
+    from hr_grade g
+    where g.year = 2022
+    group by g.emp_no
+) as g on g.emp_no = e.emp_no
+order by e.emp_no asc;
+
