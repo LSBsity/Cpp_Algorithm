5e19e5dd1701f3ea813b7382dba5154a6744ddd6
SITY <126085673+LSBsity@users.noreply.github.com>
2026-06-18 13:40:53 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 85.83 ms, Memory: 156 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,36 +1,38 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        Queue<Integer> q1 = new ArrayDeque<>();
-        Queue<Integer> q2 = new ArrayDeque<>();
-        long sum1 = 0, sum2 = 0;
-
+        int answer = 0;
+        
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+        
         for (int i = 0; i < queue1.length; i++) {
-            sum1 += queue1[i];
-            sum2 += queue2[i];
-            q1.add(queue1[i]);
-            q2.add(queue2[i]);
+            q1.offer(queue1[i]);
+            q2.offer(queue2[i]);
         }
-
-        int cnt = 0;
+        
+        long sum1 = Arrays.stream(queue1).sum();
+        long sum2 = Arrays.stream(queue2).sum();
+        
+        int limit = 4 * q1.size();
         while (sum1 != sum2) {
+            if (answer > limit) return -1;
+            
             if (sum1 > sum2) {
-                Integer q1Poll = q1.poll();
-                q2.add(q1Poll);
-                sum1 -= q1Poll;
-                sum2 += q1Poll;
+                int i2 = q1.poll();
+                sum2 += i2;
+                sum1 -= i2;
+                q2.offer(i2);
             } else {
-                Integer q2Poll = q2.poll();
-                sum2 -= q2Poll;
-                q1.add(q2Poll);
-                sum1 += q2Poll;
+                int i1 = q2.poll();
+                sum1 += i1;
+                sum2 -= i1;
+                q1.offer(i1);
             }
-            cnt++;
-            if (cnt >= 100_000_000) return -1;
+            answer++;
         }
-
-        return cnt;
+        
+        return answer;
     }
 }
