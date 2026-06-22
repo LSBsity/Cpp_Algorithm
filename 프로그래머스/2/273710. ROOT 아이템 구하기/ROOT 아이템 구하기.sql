21b72d7c8574be30a8842c4d317044bec3ab514e
SITY <126085673+LSBsity@users.noreply.github.com>
2026-06-22 16:00:09 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -1,6 +1,7 @@
-select ii.item_id,
-       ii.item_name
-from item_info ii
-    join item_tree it on ii.item_id = it.item_id
-where it.parent_item_id is null
-order by ii.item_id asc;
+select
+  i.item_id,
+  i.item_name
+from item_info i
+  join item_tree t on i.item_id = t.item_id
+where t.parent_item_id is null
+order by i.item_id asc;
