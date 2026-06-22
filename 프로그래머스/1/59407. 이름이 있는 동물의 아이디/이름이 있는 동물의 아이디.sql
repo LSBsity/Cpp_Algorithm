4e4b41e3ae2a0661277af7fd7845713980dbbfdb
SITY <126085673+LSBsity@users.noreply.github.com>
2026-06-22 12:41:02 +0900

    [level 1] Title: 이름이 있는 동물의 아이디, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/59407. 이름이 있는 동물의 아이디/이름이 있는 동물의 아이디.sql
+++ 프로그래머스/1/59407. 이름이 있는 동물의 아이디/이름이 있는 동물의 아이디.sql
@@ -1,5 +1,5 @@
--- 코드를 입력하세요
-select a.animal_id ai
-from animal_ins a
-where a.name is not null
-order by ai asc;
+select
+  ai.animal_id
+from animal_ins ai
+where ai.name is not null
+order by ai.animal_id asc;
