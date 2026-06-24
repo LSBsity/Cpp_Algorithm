34a1a4bf3f5c875a5be7e7789ba9d6874bd7b3a8
SITY <126085673+LSBsity@users.noreply.github.com>
2026-06-24 14:38:37 +0900

    [level 2] Title: 시소 짝꿍, Time: 21.18 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -1,22 +1,27 @@
 import java.util.*;
-
 class Solution {
     public long solution(int[] weights) {
         long answer = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        
+        Map<Integer, Long> map = new HashMap<>();
         for (int w : weights) {
-            answer += map.getOrDefault(w, 0);
-            if (w * 2 % 3 == 0) answer += map.getOrDefault(w * 2 / 3, 0);
-            if (w * 3 % 2 == 0) answer += map.getOrDefault(w * 3 / 2, 0);
-            if (w * 3 % 4 == 0) answer += map.getOrDefault(w * 3 / 4, 0);
-            if (w * 4 % 3 == 0) answer += map.getOrDefault(w * 4 / 3, 0);
-            if (w % 2 == 0) answer += map.getOrDefault(w / 2, 0);
-            answer += map.getOrDefault(w * 2, 0);
-
-            map.put(w, map.getOrDefault(w, 0) + 1);
+            map.put(w, map.getOrDefault(w, 0L) + 1);
         }
         
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        
+        for (int key : keys) {
+            long cnt = map.get(key);
+            answer += cnt * (cnt - 1) / 2; // 같은 무게
+        }
+        
+        for (int a : keys) {
+            for (int b : keys) {
+                if (a >= b) continue;
+                if (a * 4 == b * 2 || a * 4 == b * 3 || a * 3 == b * 2) {
+                    answer += map.get(a) * map.get(b);
+                }
+            }
+        }
         return answer;
     }
 }
