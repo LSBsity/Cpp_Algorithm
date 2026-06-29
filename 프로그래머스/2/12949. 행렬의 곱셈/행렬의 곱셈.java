de22af91f4101f3867af6a333d649ef8167b05d2
SITY <126085673+LSBsity@users.noreply.github.com>
2026-06-29 20:13:29 +0900

    [level 2] Title: 행렬의 곱셈, Time: 5.45 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -4,13 +4,12 @@ class Solution {
         
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2[0].length; j++) {
-                int sum = 0;
-                for (int k = 0; k < arr2.length; k++) {
-                    sum += arr1[i][k] * arr2[k][j];
+                for (int k = 0; k < arr1[0].length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
                 }
-                answer[i][j] = sum;
             }
         }
+        
         return answer;
     }
 }
