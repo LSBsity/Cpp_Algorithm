edb156c1da5a5b65d890880b39e9626165fac29a
SITY <126085673+LSBsity@users.noreply.github.com>
2026-06-30 17:31:36 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 3.11 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,19 +1,20 @@
 class Solution {
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
-        int answerIdx = 0;
+        
+        int idx = 0;
         for (long number : numbers) {
-            long start = 1;
-            if ((number & start) == 0) {
-                answer[answerIdx++] = ++number;
+            long prev = number;
+            
+            if ((prev & 1) == 0) {
+                number = prev + 1;
             } else {
-                number++;
-                while ((number & start) == 0) {
-                    start *= 2;
-                }
-                answer[answerIdx++] = number + start / 2 - 1;
+                number = prev + ((~prev & (prev + 1)) >> 1);
             }
+            
+            answer[idx++] = number;
         }
+        
         return answer;
     }
 }
