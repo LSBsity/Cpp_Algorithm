d9ca578b80fec4f5f11f1566ef7808c514d49d95
SITY <126085673+LSBsity@users.noreply.github.com>
2026-07-01 17:28:23 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,18 +1,20 @@
-WITH review_counts AS (
-    SELECT p.member_id, COUNT(r.review_id) AS review_count
-    FROM member_profile p
-    JOIN rest_review r ON p.member_id = r.member_id
-    GROUP BY p.member_id
-),
-max_reviewers AS (
-    SELECT member_id
-    FROM review_counts
-    WHERE review_count = (SELECT MAX(review_count) FROM review_counts)
+select
+  p.member_name,
+  r.review_text,
+  r.review_date
+from member_profile p
+  join rest_review r on r.member_id = p.member_id
+where p.member_id = (
+    select member_id
+    from rest_review
+    group by member_id
+    order by count(review_id) desc
+    limit 1
 )
-SELECT p.member_name,
-       r.review_text,
-       date_format(r.review_date, '%Y-%m-%d') as review_date
-FROM member_profile p
-JOIN rest_review r ON p.member_id = r.member_id
-WHERE p.member_id IN (SELECT member_id FROM max_reviewers)
-order by review_date asc, r.review_text asc;
+order by r.review_date asc, r.review_text asc;
+
+# select *, count(review_id)
+# from rest_review
+# group by member_id
+# order by count(review_id) desc
+# limit 1;
