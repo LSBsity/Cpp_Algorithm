270aa4fb25f020dcb32cffd5088ac0319d3cfadd
SITY <126085673+LSBsity@users.noreply.github.com>
2026-07-02 15:52:06 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 4.69 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,58 +1,63 @@
+import java.util.*;
+
 class Solution {
     public int solution(int m, int n, String[] board) {
         int answer = 0;
         
-        int[][] arr = new int[m][n];
+        char[][] map = new char[m][n];
+        int idx = 0;
+        for (String str : board) {
+            map[idx++] = str.toCharArray();
+        } 
 
-        for (int i = 0; i < m; i++) {
-            char[] chars = board[i].toCharArray();
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = chars[j];
-            }
-        }
-        
         while (true) {
             boolean[][] v = new boolean[m][n];
-            boolean check = false;
             
             for (int i = 0; i < m - 1; i++) {
                 for (int j = 0; j < n - 1; j++) {
-                    if (arr[i][j] == arr[i][j + 1] &&
-                        arr[i][j + 1] == arr[i + 1][j + 1] &&
-                        arr[i + 1][j + 1] == arr[i + 1][j] &&
-                        arr[i][j] != -1) {
-                        v[i][j] = v[i][j + 1] = v[i + 1][j + 1] = v[i + 1][j] = true;
-                        check = true;
+                    if (map[i][j] != ' ' && check(i, j, map)) {
+                        v[i][j] = v[i][j + 1] = v[i + 1][j] = v[i + 1][j + 1] = true; 
                     }
                 }
-            }
-
-            if (!check) {
-                for (int i = 0; i < m; i++) { 
-                    for (int j = 0; j < n; j++) { 
-                        if (arr[i][j] == -1) answer++;
-                    }
-                }
-                return answer;
-            }
+            }    
             
+            boolean check = true;
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (v[i][j]) arr[i][j] = -1;
+                    if (v[i][j]) {
+                        map[i][j] = ' ';
+                        answer++;
+                        check = false;
+                    }
                 }
-            }
+            } 
+            if (check) break;
             
             for (int j = 0; j < n; j++) {
-                int emptyRow = m - 1;
-                for (int i = m - 1; i >= 0; i--) {
-                    if (arr[i][j] != -1) {
-                        arr[emptyRow][j] = arr[i][j];
-                        if (emptyRow != i) arr[i][j] = -1;
-                        emptyRow--;
+                for (int i = m - 2; i >= 0; i--) {
+                    if (map[i][j] != ' ' && map[i + 1][j] == ' ') {
+                        char c = map[i][j];
+                        int k = i;
+                        
+                        while (k + 1 < m && map[k + 1][j] == ' ') k++;
+                        
+                        map[i][j] = ' ';
+                        map[k][j] = c;
                     }
                 }
             }
         }
+        
+        return answer;
     }
     
+    private static boolean check(int i, int j, char[][] map) {
+        if (map[i][j] == map[i][j + 1] &&
+            map[i][j] == map[i + 1][j] &&
+            map[i][j] == map[i + 1][j + 1]) {
+            return true;
+        }
+        
+        return false;
+    }
 }
