582f087ff863544129c14ac21de61ad36b2454b0
SITY <126085673+LSBsity@users.noreply.github.com>
2026-07-07 15:45:13 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,6 +1,9 @@
-select ai.animal_id, ai.animal_type, ai.name
+select
+  ai.animal_id,
+  ai.animal_type,
+  ai.name
 from animal_ins ai
-    join animal_outs ao on ai.animal_id = ao.animal_id
+  join animal_outs ao on ao.animal_id = ai.animal_id
 where ai.sex_upon_intake like 'Intact%' and
-      ao.sex_upon_outcome not like 'Intact%'
+  (ao.sex_upon_outcome like 'Spayed%' or ao.sex_upon_outcome like 'Neutered%')
 order by ai.animal_id asc;
