abc3f4f69f8f0b130c3dc75ffbc99cf234657f98
SITY <126085673+LSBsity@users.noreply.github.com>
2026-07-07 15:21:15 +0900

    [level 2] Title: 오픈채팅방, Time: 125.73 ms, Memory: 184 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,43 +1,42 @@
 import java.util.*;
 
 class Solution {
-    public String[] solution(String[] records) {
-        Map<String, String> nicknameMap = new HashMap<>();
-        List<Status> history = new ArrayList<>();
-
+    
+    static Map<String, String> map = new HashMap<>();
+    
+    public String[] solution(String[] records) {  
+        int cnt = 0;
+        
         for (String record : records) {
             String[] split = record.split(" ");
+            
             String cmd = split[0];
-
+            
             if (cmd.equals("Enter")) {
-                nicknameMap.put(split[1], split[2]);
-                history.add(new Status(split[1], true));
+                map.put(split[1], split[2]);
+                cnt++;
+            } else if (cmd.equals("Change")) {
+                map.put(split[1], split[2]);
             } else if (cmd.equals("Leave")) {
-                history.add(new Status(split[1], false));
-            } else {
-                nicknameMap.put(split[1], split[2]);
+                cnt++;
             }
         }
+        
+        String[] answer = new String[cnt];   
+        
+        int idx = 0;
+        for (String record : records) {
+            String[] split = record.split(" ");
 
-        String[] answer = new String[history.size()];
-        for (int i = 0, idx = 0, historySize = history.size(); i < historySize; i++, idx++) {
-            Status status = history.get(i);
-            String nickname = nicknameMap.get(status.uuid);
-
-            answer[idx] = String.format("%s님이 %s습니다.",
-                    nickname,
-                    status.isIn ? "들어왔" : "나갔");
+            String cmd = split[0];
+            
+            if (cmd.equals("Enter")) {
+                answer[idx++] = String.format("%s님이 들어왔습니다.", map.get(split[1]));
+            } else if (cmd.equals("Leave")) {
+                answer[idx++] = String.format("%s님이 나갔습니다.", map.get(split[1]));
+            }
         }
+        
         return answer;
-    }
-
-    static class Status {
-        String uuid;
-        boolean isIn;
-
-        public Status(String uuid, boolean isIn) {
-            this.uuid = uuid;
-            this.isIn = isIn;
-        }
     }
 }
