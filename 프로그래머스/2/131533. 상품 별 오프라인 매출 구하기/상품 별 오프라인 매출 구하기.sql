6b4caaa60480d1d35e0c4a37202b91fd52ea78c4
SITY <126085673+LSBsity@users.noreply.github.com>
2026-07-08 15:59:18 +0900

    [level 2] Title: 상품 별 오프라인 매출 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
+++ 프로그래머스/2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
@@ -1,7 +1,7 @@
-select p.product_code,
-       sum(os.sales_amount * p.price) as sales
-from offline_sale os
-    join product p on os.product_id = p.product_id
+select
+  p.product_code,
+  sum(p.price * s.sales_amount) as sales
+from product p
+  join offline_sale s on s.product_id = p.product_id
 group by p.product_code
-order by sum(os.sales_amount * p.price) desc, p.product_code asc;
-
+order by sales desc, p.product_code asc;
