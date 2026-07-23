91192952f8bd2a61c62e59c7af04d87e53c74827
SITY <126085673+LSBsity@users.noreply.github.com>
2026-07-23 18:13:55 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,4 +1,7 @@
-select ai.animal_id, ai.name, ai.sex_upon_intake
+select
+  ai.animal_id,
+  ai.name,
+  ai.sex_upon_intake
 from animal_ins ai
-where ai.name in ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
-order by ai.animal_id
+where ai.name in ("Lucy", "Ella", "Pickle", "Rogan", "Sabrina", "Mitty")
+order by ai.animal_id asc;
