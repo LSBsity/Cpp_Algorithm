d252f0b4d3b7a9efb432245b2714ea894c55a9a1
SITY <126085673+LSBsity@users.noreply.github.com>
2026-07-24 12:48:17 +0900

    [level 2] Title: 배달, Time: 2.78 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,66 +2,71 @@ import java.util.*;
 
 class Solution {
     
-    int[] dist;
-    boolean[] visited;
-    List<Node>[] graph;
+    static List<List<Node>> graph = new ArrayList<>();
     
-    static class Node {
-        int v;
-        int w;
-        public Node(int v, int w) {
-            this.v = v;
-            this.w = w;
-        }
-    }
-    
-    
-    public int solution(int n, int[][] road, int k) {
+    public int solution(int N, int[][] road, int K) {
         int answer = 0;
         
-        dist = new int[n + 1];
-        visited = new boolean[n + 1];
-        graph = new ArrayList[n + 1];
-        
-        for (int i = 1; i <= n; i++) {
-            dist[i] = Integer.MAX_VALUE;
-            graph[i] = new ArrayList<>();
+        for (int i = 0; i < N + 1; i++) {
+            graph.add(new ArrayList<>());
         }
         
-        for (int[] i : road) {
-            int u = i[0];
-            int v = i[1];
-            int w = i[2];
-            graph[u].add(new Node(v, w));
-            graph[v].add(new Node(u, w));
+        for (int[] r : road) {
+            int u = r[0];
+            int v = r[1];
+            int w = r[2];
+            
+            graph.get(u).add(new Node(v, w));
+            graph.get(v).add(new Node(u, w));
         }
         
-        this.dijkstra(1);
+        int[] dist = this.dijkstra(N);
         
-        for (int i = 1; i <= n; i++) {
-            if (dist[i] <= k) answer++;
+        for (int d : dist) {
+            if (d <= K) answer++;
         }
         
         return answer;
     }
     
-    private void dijkstra(int start) {
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.w - o2.w);
+    private int[] dijkstra(int n) {
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
         
-        pq.offer(new Node(start, 0));
-        dist[start] = 0;
+        dist[1] = 0;
+        
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(1, 0));
         
         while (!pq.isEmpty()) {
-            Node current = pq.poll();
-            visited[current.v] = true;
+            Node cur = pq.poll();
             
-            for (Node next : graph[current.v]) {
-                System.out.println(Arrays.toString(dist));
-                if (!visited[next.v] && dist[next.v] > current.w + next.w) {
-                    dist[next.v] = current.w + next.w;
-                    pq.offer(new Node(next.v, dist[next.v]));
+            if (cur.w > dist[cur.v]) continue;
+            
+            for (Node next : graph.get(cur.v)) {
+                int newCost = dist[cur.v] + next.w;
+                if (newCost < dist[next.v]) {
+                    dist[next.v] = newCost;
+                    pq.offer(new Node(next.v, newCost));
                 }
             }
+        }
+        
+        return dist;
+    }
+    
+    public static class Node implements Comparable<Node> {
+        int v;
+        int w;
+        
+        public Node(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+        
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.w, other.w);
         }
     }
 }
