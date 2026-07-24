1f41ee91b2a40152f1201162f75d3611d6bd8ea5
SITY <126085673+LSBsity@users.noreply.github.com>
2026-07-24 13:30:38 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -1,5 +1,6 @@
-select ai.animal_id, ai.name
+select
+  ai.animal_id,
+  ai.name
 from animal_ins ai
-where ai.name LIKE '%el%'
-    and ai.animal_type = 'Dog'
-order by ai.name;
+where ai.name like '%el%' and animal_type = 'Dog'
+order by ai.name asc, ai.animal_id asc
