1afa902abbbb52c4be9c8f81117c0cffb4e84f7f
SITY <126085673+LSBsity@users.noreply.github.com>
2026-07-25 02:03:29 +0900

    [level 3] Title: 가장 먼 노드, Time: 20.62 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,59 +2,70 @@ import java.util.*;
 
 class Solution {
     
-    static List<Integer>[] graph;
-    static boolean[] visited;
-    static int max = Integer.MIN_VALUE;
+    static List<List<Node>> graph = new ArrayList<>();
     
-    public int solution(int n, int[][] edges) {
-        graph = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
-        
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+    public int solution(int n, int[][] edge) {
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
         }
         
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
+        for (int[] e : edge) {
+            int u = e[0];
+            int v = e[1];
             
-            graph[u].add(v);
-            graph[v].add(u);
+            graph.get(u).add(new Node(v, 1));
+            graph.get(v).add(new Node(u, 1));
         }
         
-        return go(1);
+        return go(n);
     }
     
-    
-    private static int go(int start) {
-        Queue<int[]> q = new LinkedList<>();
-        List<int[]> list = new ArrayList<>();
+    private int go(int n) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
         
-        visited[start] = true;
-        q.offer(new int[] {start, 1});
-        list.add(new int[] {start, 1});
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[1] = 0;
         
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            max = Math.max(max, cur[1]);  
+        pq.offer(new Node(1, 0));
+        int maxDist = -1;
+        
+        while(!pq.isEmpty()) {
+            Node cur = pq.poll();
             
-            for (int next : graph[cur[0]]) {
-                if (visited[next]) continue;
+            if (cur.w > dist[cur.v]) continue;
+            
+            for (Node next : graph.get(cur.v)) {
                 
-                visited[next] = true;
-                q.offer(new int[] {next, cur[1] + 1});
-                list.add(new int[] {next, cur[1] + 1});
-
+                int newCost = dist[cur.v] + next.w;
+                if (newCost < dist[next.v]) {
+                    dist[next.v] = newCost;
+                    maxDist = Math.max(maxDist, dist[next.v]);
+                    pq.offer(new Node(next.v, newCost));
+                }
             }
-            
         }
+    
         int cnt = 0;
-        for (int[] i : list) {
-            if (i[1] == max) {
-                cnt++;
-            }
+        for (int d : dist) {
+            if (d == maxDist) cnt++;
         }
         
         return cnt;
+    }
+    
+    public static class Node implements Comparable<Node> {
+        int v;
+        int w;
+        
+        public Node(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+        
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.w, other.w);
+        } 
     }
 }
