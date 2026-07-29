aff7a3f1afc0bd6a0a1d2c365eae61b9744cfd4a
SITY <126085673+LSBsity@users.noreply.github.com>
2026-07-29 20:53:20 +0900

    [level 2] Title: 리코쳇 로봇, Time: 2.77 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,76 +1,79 @@
 import java.util.*;
 
 class Solution {
-
+    
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
     
-    char[][] map;
-    int[][] visited;
-    int[][] pos = new int[2][2];
+    static int[][] map;    
+
+    static int[][] coords = {{0, 0}, {0, 0}};
     
     public int solution(String[] board) {
-        int answer = -1;
+        int answer = 0;
         
-        map = new char[board.length][board[0].length()];
-        visited = new int[board.length][board[0].length()];
+        map = new int[board.length][board[0].length()];
 
-        for (int i = 0; i < board.length; i++) {
-            char[] chars = board[i].toCharArray();
-            for (int j = 0; j < chars.length; j++) {
-                map[i][j] = chars[j];
-                if (map[i][j] == 'R') {
-                    pos[0][0] = i;
-                    pos[0][1] = j;
-                } else if (map[i][j] == 'G') {
-                    pos[1][0] = i;
-                    pos[1][1] = j;
+        for (int i = 0; i < map.length; i++) {
+            String str = board[i];
+            for (int j = 0; j < map[0].length; j++) {
+                char c = str.charAt(j);
+                if (c == 'D') {
+                    map[i][j] = 1;
+                } else if (c == 'G') {
+                    map[i][j] = 2;
+                    coords[1][0] = i;
+                    coords[1][1] = j;
+                } else if (c == 'R') {
+                    coords[0][0] = i;
+                    coords[0][1] = j;
                 }
             }
         }
         
-        Queue<Position> q = new LinkedList<>();
-        q.offer(new Position(pos[0][0], pos[0][1], 0));
-        
-        while (!q.isEmpty()) {
-            Position current = q.poll();
-            
-            if (current.x == pos[1][0] && current.y == pos[1][1]) {
-                answer = current.count;
-                break;
-            }
-            
-            for (int i = 0; i < 4; i++) {
-                int nx = current.x;
-                int ny = current.y;
-                while (true) {
-                    int tx = nx + dx[i];
-                    int ty = ny + dy[i];
-                    if (tx < 0 || ty < 0 || tx >= board.length || ty >= board[0].length()) break;
-                    if (map[tx][ty] == 'D') break;
-                    nx = tx;
-                    ny = ty;
-                } 
-
-                if (visited[nx][ny] == 0 || visited[nx][ny] > current.count + 1) {
-                    visited[nx][ny] = current.count + 1;
-                    q.offer(new Position(nx, ny, current.count + 1));
-                }
-            }
-        }
+        answer = go();
         
         return answer;
     }
     
-    static class Position {
-        int x;
-        int y;
-        int count;
-        public Position(int x, int y, int count) {
-            this.x = x;
-            this.y = y;
-            this.count = count;
-        }
-    }
+    private static int go() {
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[map.length][map[0].length];
+        
+        q.offer(new int[] {coords[0][0], coords[0][1], 0});
+        visited[coords[0][0]][coords[0][1]] = true;
+        
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            
+            if (cur[0] == coords[1][0] && cur[1] == coords[1][1]) {
+                return cur[2];
+            }
 
+            for (int i = 0; i < 4; i++) {
+                int pos = 0;
+                int nx = cur[0];
+                int ny = cur[1];
+                
+                while (true) {
+                    nx += dx[i];
+                    ny += dy[i];
+                    
+                    if (nx < 0 || ny < 0 || nx >= map.length || ny >= map[0].length || 
+                        map[nx][ny] == 1) {
+                        nx -= dx[i];
+                        ny -= dy[i];
+                        break;
+                    }
+                }
+                
+                if (!visited[nx][ny]) {
+                    q.offer(new int[] {nx, ny, cur[2] + 1});    
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+                    
+        return -1;
+    } 
 }
