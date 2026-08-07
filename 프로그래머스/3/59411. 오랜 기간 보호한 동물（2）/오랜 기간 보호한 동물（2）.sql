1cd5054817e9a6c3fab27346ed2372e4af83fcc9
SITY <126085673+LSBsity@users.noreply.github.com>
2026-08-07 19:03:04 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,5 +1,8 @@
-select ao.animal_id, ao.name
+select
+  ai.animal_id,
+  ai.name
 from animal_ins ai
-    join animal_outs ao on ai.animal_id = ao.animal_id
-order by timestampdiff(second, ai.datetime, ao.datetime) desc
+  join animal_outs ao on ai.animal_id = ao.animal_id
+where ao.datetime is not null
+order by datediff(ao.datetime, ai.datetime) desc
 limit 2;
