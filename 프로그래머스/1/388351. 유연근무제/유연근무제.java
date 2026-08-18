851208492a8a19f0df587fcdd12a91fe8093740a
SITY <126085673+LSBsity@users.noreply.github.com>
2026-08-18 17:00:02 +0900

    [level 1] Title: 유연근무제, Time: 0.32 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.java
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -1,41 +1,26 @@
 import java.util.*;
 
 class Solution {
-    
-    static int[][] holydays = new int[][] {
-        {5, 6},
-        {4, 5},
-        {3, 4},
-        {2, 3},
-        {1, 2},
-        {0, 1},
-        {0, 6}
-    };
-    
     public int solution(int[] schedules, int[][] timelogs, int startday) {
         int answer = 0;
         
-        for (int i = 0; i < schedules.length; i++) {
-            int count = 0;
-            
-            int hour = schedules[i] / 100;
-            int minutes = schedules[i] % 100;
-            minutes += 10;
-            if (minutes >= 60) {
-                hour++;
-                minutes %= 60;
+        for (int i = 0; i < timelogs.length; i++) {
+            int should = schedules[i] + 10;
+            if (should % 100 >= 60) {
+                should += 40;
             }
-            int limitTime = hour * 100 + minutes;
             
-            for (int j = 0; j < 7; j++) {
-                if (j != holydays[startday - 1][0] && j != holydays[startday - 1][1] && timelogs[i][j] <= limitTime) {
-                    count++;
+            boolean check = true;
+            for (int j = 0, day = startday; j < timelogs[i].length; j++, day = (day % 7) + 1) {
+                if (day >= 6) continue;
+                    
+                if (should < timelogs[i][j]) {
+                    check = false;
+                    break;
                 }
             }
             
-            if (count == 5) {
-                answer++;
-            }
+            if (check) answer++;
         }
         
         return answer;
