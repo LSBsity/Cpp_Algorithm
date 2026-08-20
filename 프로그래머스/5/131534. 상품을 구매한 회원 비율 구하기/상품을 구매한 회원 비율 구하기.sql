39e95cc8e22166810687d97f2c2e7b9db2be195e
SITY <126085673+LSBsity@users.noreply.github.com>
2026-08-20 13:38:19 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,20 +1,15 @@
-select year(s.sales_date) as year,
-       month(s.sales_date) as month,
-       count(distinct s.user_id) as purchased_users,
-       round(count(distinct s.user_id) / 
-             (
-                 select count(*)
-                 from user_info sub_i
-                 where year(sub_i.joined) = 2021
-             ), 1) as puchased_ratio
-from user_info i
-    join online_sale s on i.user_id = s.user_id
-where year(i.joined) = 2021
-group by year, month
+select
+  year(os.sales_date) as year,
+  month(os.sales_date) as month,
+  count(distinct ui.user_id) as purchased_users,
+  round(count(distinct ui.user_id) /
+  (select count(user_id) from user_info where year(joined) = 2021), 1) as puchased_ratio
+from user_info ui
+  join online_sale os on ui.user_id = os.user_id
+where ui.user_id in (
+    select user_id
+    from user_info
+    where year(joined) = 2021
+)
+group by year(os.sales_date), month(os.sales_date)
 order by year asc, month asc;
-
-# select *
-# from user_info i
-#     join online_sale s on i.user_id = s.user_id
-# where year(i.joined) = 2021
-# order by i.user_id asc;
