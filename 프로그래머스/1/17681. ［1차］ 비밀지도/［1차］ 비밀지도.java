63dbc9692099e234ac66e2feb14282a6b40ed2c6
SITY <126085673+LSBsity@users.noreply.github.com>
2026-08-21 16:46:05 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.29 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,13 +1,23 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-
-        for (int i = 0; i < n; i++) {
-            answer[i] = String.format("%" + n + "s", Integer.toBinaryString(arr1[i] | arr2[i]))
-                            .replace('1', '#')
-                            .replace('0', ' ');
+        String[] answer = new String[arr1.length];
+        
+        for (int i = 0, idx = 0; i < arr1.length; i++) {
+            int or = arr1[i] | arr2[i];
+            
+            String bitStr = Integer.toBinaryString(or)
+                .replaceAll("1", "#")
+                .replaceAll("0", " ");
+            
+            if (bitStr.length() < n) {
+                String padding = " ".repeat(n - bitStr.length());
+                bitStr = padding + bitStr;
+            }
+            
+            answer[idx++] = bitStr;
         }
-
         return answer;
     }
 }
+//10110
+//01110
