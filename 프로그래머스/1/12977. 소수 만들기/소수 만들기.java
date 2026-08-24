3a2ecada1315450ee120f436ad42aec6acd7569c
SITY <126085673+LSBsity@users.noreply.github.com>
2026-08-24 16:02:46 +0900

    [level 1] Title: 소수 만들기, Time: 2.03 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,27 +1,27 @@
 class Solution {
     public int solution(int[] nums) {
-        int count = 0;
-        for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
+        int answer = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
-                    int sum = nums[i] + nums[j] + nums[k];
-                    if (check(sum)) {
-                        count++;
+                    if (go(nums[i] + nums[j] + nums[k])) {
+                        answer++;
                     }
                 }
             }
         }
-        return count;
+
+        return answer;
     }
-
-    private boolean check(int num) {
-        if (num == 1) return false;
-        if (num == 2) return true;
-
-        for (int i = 2; i <= num / 2; i++) {
-            if (num % i == 0) return false;
+    
+    private boolean go(int n) {
+        if (n <= 1) return false;
+        
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
         }
-
+        
         return true;
     }
 }
