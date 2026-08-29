8f19c01ef6e083cc36b768cfdccb265c72b7964e
SITY <126085673+LSBsity@users.noreply.github.com>
2026-08-29 18:23:55 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 8.24 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,34 +1,40 @@
-import java.util.*;
 import java.time.*;
+import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        List<Integer> answer = new ArrayList<>();
-        Map<String, Integer> m = new HashMap<>();
+        List<Integer> answer = new ArrayList<>(); 
+        Map<String, Integer> map = new HashMap<>();
         
-        String[] todays = today.split("\\.");
-        LocalDate now = LocalDate.of(Integer.parseInt(todays[0]), Integer.parseInt(todays[1]), Integer.parseInt(todays[2]));
-        
-        for (var term : terms) {
-            String[] p = term.split(" ");
-            m.put(p[0], Integer.parseInt(p[1]));
+        for(String term : terms) {
+            String[] s = term.split(" ");
+            map.put(s[0], Integer.parseInt(s[1]));
         }
+        
+        String[] split = today.split("\\.");
+        LocalDate now = LocalDate.of(
+            Integer.parseInt(split[0]),
+            Integer.parseInt(split[1]),
+            Integer.parseInt(split[2])
+        );
 
-        int count = 1;
-        for (var privacy : privacies) {
-            String[] p = privacy.split(" ");
-            String[] d = p[0].split("\\.");
-            String t = p[1];
-            LocalDate termDay = LocalDate.of(Integer.parseInt(d[0]), Integer.parseInt(d[1]), Integer.parseInt(d[2]));
-            LocalDate expired = termDay.plusMonths(m.get(t)).minusDays(1);
-            if (expired.isBefore(now)) {
-                answer.add(count);
+        for (int i = 0; i < privacies.length; i++) {
+            String[] s = privacies[i].split("[. ]");
+            
+            LocalDate day = LocalDate.of(
+                Integer.parseInt(s[0]),
+                Integer.parseInt(s[1]),
+                Integer.parseInt(s[2])
+            );
+            
+            int termMonth = map.get(s[3]);
+            LocalDate expired = day.plusMonths(termMonth);
+            
+            if (!now.isBefore(expired)) {
+                answer.add(i + 1);
             }
-            count++;
         }
-
-        return answer.stream()
-            .mapToInt(i -> i)
-            .toArray();
+        
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
