66bd0c6c6d4eb87209e7471d960cd82ab497fd2f
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-01 17:23:29 +0900

    [level 1] Title: 소수 찾기, Time: 9.57 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,27 +1,28 @@
 import java.util.*;
 
 class Solution {
+
+    public int solution(int n) {
+        return eratos(n);
+    }
     
-    private int eratos(int n) {
-        boolean[] isPrime = new boolean[n + 1];
-        int count = 0;
+    public int eratos(int n) {
+        boolean[] arr = new boolean[n + 1];
         
-        for (int i = 2; i * i <= n; i++) {
-            if (!isPrime[i]) {
-                for (int j = i * i; j <= n; j += i) {
-                    isPrime[j] = true;
-                }                
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (arr[i]) continue;
+            
+            for (int j = i * i; j <= n; j += i) {
+                arr[j] = true;
             }
         }
         
-        for (int i = 2; i <= n; i++) {
-            if (!isPrime[i]) count++;
+        int cnt = 0;
+        for(int i = 2; i <= n; i++) {
+            if (!arr[i]) cnt++;
         }
         
-        return count;
-    } 
-    
-    public int solution(int n) {
-        return this.eratos(n);
+        return cnt;
     }
+
 }
