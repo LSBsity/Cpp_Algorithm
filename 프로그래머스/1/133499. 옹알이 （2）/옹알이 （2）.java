421699702cbd18a1dd1227878a7aa986ca97a386
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-01 18:10:10 +0900

    [level 1] Title: 옹알이 (2), Time: 14.06 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,23 +1,36 @@
-class Solution {
-    public int solution(String[] babbling) {
-        String[] words = {"aya", "ye", "woo", "ma"};
-        int count = 0;
+import java.util.*;
 
+class Solution {
+    
+    static final String[] BABBLING = {"ye", "woo", "ma", "aya"};
+    
+    public int solution(String[] babbling) {
+        int answer = 0;
+        
         for (String str : babbling) {
-            String temp = str;
             
-            for (String word : words) {
-                if (temp.contains(word + word)) { 
-                    break;
+            for (String bab : BABBLING) {
+                int repeat = str.length() / bab.length();
+                while (repeat >= 2) {
+                    String temp = bab.repeat(repeat);
+                    str = str.replaceAll(temp, "1");
+                    repeat--;
                 }
-                temp = temp.replace(word, " ");
-                if (temp.trim().isEmpty()) {
-                    count++;
+            }
+            
+            for (String bab : BABBLING) {
+                str = str.replaceAll(bab, "2");
+            }
+            boolean check = true;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) != '2') {
+                    check = false;
                     break;
                 }
             }
+            if (check) answer++;
         }
         
-        return count;
+        return answer;
     }
 }
