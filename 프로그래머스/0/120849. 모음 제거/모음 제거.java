2efbe4436504394f21ebedbe3bbebf584d83bbe2
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-03 17:00:08 +0900

    [level 0] Title: 모음 제거, Time: 0.11 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,9 +1,11 @@
 class Solution {
+    
+    static final String[] vowels = {"a", "e", "i", "o", "u"};
+    
     public String solution(String my_string) {
-        StringBuilder answer = new StringBuilder();
-        for (char c : my_string.toCharArray()) {
-            if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') answer.append(c);
+        for (String vowel : vowels) {
+            my_string = my_string.replaceAll(vowel, "");
         }
-        return answer.toString();
+        return my_string;
     }
 }
