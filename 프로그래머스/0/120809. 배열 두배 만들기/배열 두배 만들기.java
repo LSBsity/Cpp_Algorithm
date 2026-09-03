488f0720f604533ac78fd04b278627cfce9527a6
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-03 21:55:53 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -1,8 +1,9 @@
-import java.util.stream.IntStream;
-
 class Solution {
-    public int[] solution(int[] array) {
-        IntStream.range(0, array.length).forEach(i -> array[i] *= 2);
-        return array;
+    public int[] solution(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] *= 2;
+        }
+        
+        return numbers;
     }
 }
