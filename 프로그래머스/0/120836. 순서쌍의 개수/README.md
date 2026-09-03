b445b7379cf78fe48fdb051edcf8a56c4763e38a
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-03 16:40:45 +0900

    [level 0] Title: 순서쌍의 개수, Time: 2.64 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/README.md
+++ 프로그래머스/0/120836. 순서쌍의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 순서쌍의 개수 - 120836 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120836?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120836) 
 
 ### 성능 요약
 
-메모리: 75.4 MB, 시간: 5.64 ms
+메모리: 74.2 MB, 시간: 2.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 04일 11:22:42
+2026년 09월 03일 16:40:44
 
 ### 문제 설명
 
--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,9 +1,13 @@
 class Solution {
     public int solution(int n) {
-        int count = 0;
-        for (int i = 1; i <= n / 2; i++) {
-            if (n % i == 0) count++;
+        int answer = 1;
+        int a = 1;
+        int b = n;
+        while (a < b) {
+            a++;
+            if (n % a == 0) answer++;
         }
-        return count + 1;
+        
+        return answer;
     }
 }
