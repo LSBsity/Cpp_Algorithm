078290026978431eb9e6da60410ad9ac38db5cef
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-03 15:11:28 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.08 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -1,5 +1,5 @@
 class Solution {
     public String solution(String my_string, String letter) {
-        return my_string.replace(letter, "");
+        return my_string.replaceAll(letter, "");
     }
 }
