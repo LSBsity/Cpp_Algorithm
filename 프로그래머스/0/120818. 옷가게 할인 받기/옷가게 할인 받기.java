7287daedebc07e471a32920634216df6e2e93ff8
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-03 20:59:01 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,13 +1,14 @@
 class Solution {
     public int solution(int price) {
-        double a = 0;
-        if (price >= 500_000) {
-            a = 0.2;
-        } else if (price >= 300_000) {
-            a = 0.1;
-        } else if (price >= 100_000) {
-            a = 0.05;
-        }
-        return (int) (price * (1.0 - a));
+        int discountRate = getDiscountRate(price);
+        return price * (100 - discountRate) / 100;
     }
+    
+    private int getDiscountRate(int price) {
+        if (price >= 500_000) return 20;
+        if (price >= 300_000) return 10;
+        if (price >= 100_000) return 5;
+        return 0;
+    }
+    
 }
