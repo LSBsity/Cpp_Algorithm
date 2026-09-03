d4dbb3cad34ed65c9cda536af4c0c48ec76f6a83
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-03 20:22:16 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,10 +1,13 @@
 class Solution {
     public int solution(int n) {
-        int sum = 0;
+        int answer = 0;
+        
         while (n > 0) {
-            sum += n % 10;
+            int rem = n % 10;
             n /= 10;
+            answer += rem;
         }
-        return sum;
+        
+        return answer;
     }
 }
