6df6dd94dde53d5c971c0aada65cc0be131198bf
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-03 16:07:31 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,9 +1,9 @@
-import java.util.stream.IntStream;
-
 class Solution {
     public int solution(int n) {
-        return IntStream.rangeClosed(2, n)
-                .filter(i -> i % 2 == 0)
-                .sum();
+        int answer = 0;
+        for (int i = 2; i <= n; i += 2) {
+            answer += i;
+        }
+        return answer;
     }
 }
