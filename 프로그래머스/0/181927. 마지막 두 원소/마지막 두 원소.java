e5b968286a72ee23013b607a0ec2fd64c10e357e
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-04 20:06:49 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,13 +1,20 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] num_list2 = Arrays.copyOf(num_list, num_list.length + 1);
-        if (num_list[num_list.length - 1] > num_list[num_list.length - 2]) {
-            num_list2[num_list2.length - 1] = num_list[num_list.length - 1] - num_list[num_list.length - 2];
-        } else {
-            num_list2[num_list2.length - 1] = num_list[num_list.length - 1] * 2;
+        int[] answer = new int[num_list.length + 1];
+            
+        for (int i = 0; i < num_list.length; i++) {
+            answer[i] = num_list[i];
         }
-        return num_list2;
+        
+        int last = num_list[num_list.length - 1];
+        int before = num_list[num_list.length - 2];
+        
+        if (last > before) {
+            answer[answer.length - 1] = last - before;
+        } else {
+            answer[answer.length - 1] = last * 2;
+        }
+    
+        return answer;
     }
 }
