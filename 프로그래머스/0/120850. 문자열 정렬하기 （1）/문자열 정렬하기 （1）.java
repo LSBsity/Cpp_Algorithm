c5184292590f71dfdb2312fb5049c143660bf174
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-04 20:46:11 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 2.70 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -1,11 +1,16 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(String my_string) {
-        return Arrays.stream(my_string.split(""))
-                .filter(i -> i.charAt(0) >= '0' && i.charAt(0) <= '9')
-                .mapToInt(Integer::parseInt)
-                .sorted()
-                .toArray();
+        List<Character> list = new ArrayList<>();
+        
+        char[] chars = my_string.toCharArray();
+        Arrays.sort(chars);
+        
+        for (char c : chars) {
+            if (c - '0' < 10) list.add(c);
+        }
+        
+        return list.stream().mapToInt(i -> i - '0').toArray();
     }
 }
