218b20cf7cc72fe659d5ed20af94b121d1a3ed30
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-04 20:37:02 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,5 +1,15 @@
 class Solution {
     public int solution(int[] box, int n) {
-        return (box[0] / n) * (box[1] / n) * (box[2] / n);
+        int answer = 1;
+        
+        int row = box[0];
+        int col = box[1];
+        int height = box[2];
+        
+        answer *= row / n;
+        answer *= col / n;
+        answer *= height / n;
+        
+        return answer;
     }
 }
