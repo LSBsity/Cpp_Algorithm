7776b26e9c879d725d57ed53b938480f8745211c
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-04 15:55:19 +0900

    [level 0] Title: 수 조작하기 1, Time: 4.51 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,14 +1,21 @@
 class Solution {
     public int solution(int n, String control) {
+        int answer = n;
+        
         for (char c : control.toCharArray()) {
-            n += switch (c) {
-                case 'w' -> 1;
-                case 's' -> -1;
-                case 'd' -> 10;
-                case 'a' -> -10;
-                default -> 0;
-            };
+            answer += get(c);
         }
-        return n;
+        
+        return answer;
+    }
+    
+    public int get(char c) {
+        return switch (c) {
+            case 'w' -> 1;
+            case 's' -> -1;
+            case 'd' -> 10;
+            case 'a' -> -10;
+            default -> 0;
+        };
     }
 }
