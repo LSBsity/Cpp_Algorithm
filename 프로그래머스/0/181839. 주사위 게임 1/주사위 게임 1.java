075cec4a99ebfccfd82f2bdb5bc52a224e89de2a
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-05 14:05:56 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -1,14 +1,19 @@
-import java.util.function.IntPredicate;
-
 class Solution {
     public int solution(int a, int b) {
-        IntPredicate isOdd = x -> x % 2 == 1;
-        if (isOdd.test(a) && isOdd.test(b)) {
-            return a * a + b * b;
-        } else if (isOdd.test(a) || isOdd.test(b)) {
-            return 2 * (a + b);
+        int answer = 0;
+        
+        boolean aEven = a % 2 == 0;
+        boolean bEven = b % 2 == 0;
+        
+        if (aEven && bEven) {
+            answer = Math.abs(a - b);
+        } else if (!aEven && !bEven) {
+            answer = (a * a) + (b * b);
         } else {
-            return Math.abs(a - b);
+            answer = 2 * (a + b);
         }
+        
+        
+        return answer;
     }
 }
