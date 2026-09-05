6eee095ce77817c221f1040432d6a8045145a84b
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-05 19:22:08 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -1,11 +1,13 @@
-import java.util.stream.IntStream;
-
 class Solution {
     public int solution(int[] arr, int idx) {
-        return IntStream.range(0, arr.length)
-                .skip(idx)
-                .filter(i -> arr[i] == 1)
-                .findFirst()
-                .orElse(-1);
+        int answer = -1;
+        
+        for (int i = idx; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                return i;
+            }
+        }
+        
+        return answer;
     }
 }
