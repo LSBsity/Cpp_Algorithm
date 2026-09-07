9582845ff67cfdda83b3a6dba63fb6e8ae5e9b0d
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-07 18:02:34 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,9 +1,13 @@
-import java.util.stream.IntStream;
-
 class Solution {
     public int solution(int n) {
-        return IntStream.rangeClosed(1, n)
-                .filter(i -> n % i == 0)
-                .sum();
+        int answer = 0;
+        
+        for (int i = 1; i <= n; i++) {
+             if (n % i == 0) {
+                 answer += i;
+             }
+        }
+        
+        return answer;
     }
 }
