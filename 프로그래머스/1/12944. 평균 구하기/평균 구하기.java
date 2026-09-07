c1e518ff403ffbc5fdedb1d5f444b2a9d968890c
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-07 19:38:35 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,9 +1,11 @@
-import java.util.Arrays;
-
 class Solution {
     public double solution(int[] arr) {
-        return Arrays.stream(arr)
-                .average()
-                .getAsDouble();
+        double answer = 0;
+        
+        for (int i : arr) {
+            answer += i;
+        }
+        
+        return answer / arr.length;
     }
 }
