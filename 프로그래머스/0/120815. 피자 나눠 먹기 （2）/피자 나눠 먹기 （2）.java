c869c92057d269a30908bc1b6b25831545453515
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-07 20:07:45 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,14 +1,13 @@
 class Solution {
-    int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
-    }
-
-    int lcm(int b) {
-        return 6 * b / gcd(6, b);
-    }
-
     public int solution(int n) {
-        return lcm(n) / 6;
+        return lcm(n, 6) / 6;
     }
+    
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+    
+    private int lcm(int a,int b) {
+        return a * b / gcd(a, b);
+    } 
 }
