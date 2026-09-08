3bf001761442beee1b7fe5069ac65232fdbcc5e5
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-08 19:26:05 +0900

    [level 0] Title: 문자열 묶기, Time: 3.45 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -1,18 +1,16 @@
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int solution(String[] strArr) {
-        Map<Integer, Integer> map = new HashMap<>();
-
+        int answer = 0;
+        int[] arr = new int[31];
+        
         for (String str : strArr) {
-            map.put(str.length(), map.getOrDefault(str.length(), 0) + 1);
+            arr[str.length()]++;
         }
-
-        return map.values()
-                .stream()
-                .max(Comparator.comparing(i -> i))
-                .orElseThrow();
+        
+        for (int i : arr) {
+            answer = Math.max(answer, i);
+        }
+        
+        return answer;
     }
 }
