a74563fe679d2fd19b837fa74a21df21bccbd185
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-08 18:42:17 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,7 +1,16 @@
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 45;
-        for (int number: numbers) answer -= number;
+        int answer = 0;
+        int[] arr = new int[10];
+        
+        for (int number : numbers) {
+            arr[number]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            if (arr[i] == 0) answer += i;
+        }
+        
         return answer;
     }
 }
