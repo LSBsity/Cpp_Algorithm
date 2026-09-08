ba9e459910fd4843f2c9b72862eeb223a35a5811
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-08 19:50:23 +0900

    [level 0] Title: 세 개의 구분자, Time: 44.65 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,10 +1,11 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public String[] solution(String myStr) {
-        String[] arr = Arrays.stream(myStr.split("[abc]"))
-                .filter(i -> !i.isEmpty())
-                .toArray(String[]::new);
-        return arr.length != 0 ? arr : new String[]{"EMPTY"};
+        String[] answer = Arrays.stream(myStr.split("[abc]"))
+            .filter(s -> !s.isEmpty())
+            .toArray(String[]::new);
+        
+        return answer.length == 0 ? new String[] {"EMPTY"} : answer;
     }
 }
