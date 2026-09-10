6c342b37e1e171a4c2938a07bcbc1896081a94f5
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-10 15:31:46 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 2.33 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -1,13 +1,16 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        for (int i = 0; i < queries.length; i++) {
-            for (int k = 0; k < arr.length; k++) {
-                if (queries[i][0] <= k && k <= queries[i][1] && k % queries[i][2] == 0) {
-                    arr[k]++;
-                }
+        
+        for (int[] query : queries) {
+            int s = query[0];
+            int e = query[1];
+            int k = query[2];
+            
+            for (int i = s; i <= e; i++) {
+                if (i % k == 0) arr[i]++;
             }
         }
-
+        
         return arr;
     }
 }
