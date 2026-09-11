f710f3ce3cda7e32cf1325eb58f7f518bf7db40e
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-11 20:02:00 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.05 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -3,13 +3,12 @@ import java.util.Arrays;
 class Solution {
     public String[] solution(String[] str_list) {
         for (int i = 0; i < str_list.length; i++) {
-            String str = str_list[i];
-            if (str.equals("l")) {
+            if (str_list[i].equals("l")) {
                 return Arrays.copyOfRange(str_list, 0, i);
-            } else if (str.equals("r")) {
+            } else if (str_list[i].equals("r")) {
                 return Arrays.copyOfRange(str_list, i + 1, str_list.length);
             }
         }
-        return new String[]{};
+        return new String[0];
     }
 }
