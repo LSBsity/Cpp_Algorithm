4b97f6c06a561450ce808c028165d9ac3ec835fe
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-16 12:33:05 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,15 +1,21 @@
+import java.util.*;
+
 class Solution {
+    
+    static int answer = 0;
     public int solution(int[] number) {
-        int answer = 0;
-        
-        for (int i = 0; i < number.length; i++) {
-            for (int j = i + 1; j < number.length; j++) {
-                for (int k = j + 1; k < number.length; k++) {
-                    if (number[i] + number[j] + number[k] == 0) answer++;
-                }
-            }
+        go(number, new boolean[number.length], 0, 0, 0);
+        return answer;
+    }
+    
+    private void go(int[] number, boolean[] visited, int r, int depth, int sum) {
+        if (depth == 3) {
+            if (sum == 0) answer++; 
+            return;
         }
         
-        return answer;
+        for (int i = r; i < number.length; i++) {
+            go(number, visited, i + 1, depth + 1, sum + number[i]);
+        }
     }
 }
