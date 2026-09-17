4274a1a58bd6e422230658cc12aa3b1151373124
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-17 15:35:40 +0900

    [level 1] Title: 시저 암호, Time: 1.12 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,23 +1,18 @@
 class Solution {
-
     public String solution(String s, int n) {
-        char[] chars = s.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            if (chars[i] != ' ') {
-                if (Character.isUpperCase(chars[i])) {
-                    chars[i] += n;
-                    if (!Character.isAlphabetic(chars[i]) || Character.isLowerCase(chars[i])) {
-                        chars[i] -= 26;
-                    }
-                } else {
-                    chars[i] += n;
-                    if (!Character.isAlphabetic(chars[i]) || Character.isUpperCase(chars[i])) {
-                        chars[i] -= 26;
-                    }
-                }
+        char[] answer = s.toCharArray();
+        
+        for (int i = 0; i < answer.length; i++) {
+            if (answer[i] == ' ') continue;
+            
+            char c = (char) ((int) answer[i] + (n % 26));
+            if (Character.isUpperCase(answer[i]) && Character.isLowerCase(c) || !Character.isAlphabetic(c)) {
+                c -= 26;
             }
+            
+            answer[i] = c;
         }
-
-        return new String(chars);
+        
+        return new String(answer);
     }
 }
