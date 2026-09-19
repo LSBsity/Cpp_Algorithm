68eed7ab423f8b67eca3e5768e5cef8469324ed9
SITY <126085673+LSBsity@users.noreply.github.com>
2026-09-19 17:49:05 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -1,31 +1,37 @@
+import java.util.*;
+
 class Solution {
-
-    private static final int[] dx = new int[]{0, -1, 0, 1, -1, -1, 1, 1};
-    private static final int[] dy = new int[]{1, 0, -1, 0, 1, -1, 1, -1};
-
+    
+    static int[] dx = {0, -1, -1, -1, 0, 1, 1, 1, 0};
+    static int[] dy = {0, -1, 0, 1, 1, 1, 0, -1, -1};
+    
     public int solution(int[][] board) {
-        int count = board.length * board.length;
+        int answer = 0;
         
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                if (board[i][j] == 1) {
-                    count--;
-                    for (int k = 0; k < 8; k++) {
-                        int x = i + dx[k];
-                        int y = j + dy[k];
-                        if (x >= 0 && x < board.length && y >= 0 && y < board.length) {
-                            if (board[x][y] != 1) {
-                                if (board[x][y] != -1) {
-                                    count--;
-                                }
-                                board[x][y] = -1;
-                            }
-                        }
-                    }
+        int n = board.length, m = board[0].length;
+        boolean[][] notSafe = new boolean[n][m];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 0) continue;
+                
+                for (int dir = 0; dir < 9; dir++) {
+                    int nx = i + dx[dir];    
+                    int ny = j + dy[dir];
+                    
+                    if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                    notSafe[nx][ny] = true;
                 }
             }
         }
-
-        return count;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!notSafe[i][j]) answer++;
+            }
+        }
+        
+        
+        return answer;
     }
 }
